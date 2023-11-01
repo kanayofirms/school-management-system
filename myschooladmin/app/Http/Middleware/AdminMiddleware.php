b3dc7ce25b30c0d4abd8932f5e35125bb4e3b737
kanayofirms <kanyeze@gmail.com>
2023-11-01 06:30:53 +0100

    Create middleware for admin route

---
+++ myschooladmin/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!empty(Auth::check()))
+        {
+            if(Auth::user->user_type == 1)
+            {
+                return $next($request);
+            }
+            else
+            {
+                
+            }   
+        }
+        else{
+            Auth::logout();
+            return redirect(url(''));
+        }
+       
+    }
+}
