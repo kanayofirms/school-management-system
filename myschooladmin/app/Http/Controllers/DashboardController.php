fb0d435b1ced7a443f81550f88de52f05b40931a
kanayofirms <kanyeze@gmail.com>
2023-11-13 19:13:42 +0100

    Update controller for role wise dashboard

--- myschooladmin/app/Http/Controllers/DashboardController.php
+++ myschooladmin/app/Http/Controllers/DashboardController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        if(Auth::user()->user_type == 1)
+        {
+            return view('admin.dashboard');
+        }
+        else if(Auth::user()->user_type == 2)
+        {
+            return view('teacher.dashboard');
+        }
+        else if(Auth::user()->user_type == 3)
+        {
+            return view('student.dashboard');
+        }
+        else if(Auth::user()->user_type == 4)
+        {
+            return view('parent.dashboard');
+        }
+        
+    }
 }
