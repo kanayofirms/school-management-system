ad26cd02b5702dd49e4d815eba15fdb642daf647
kanayofirms <kanyeze@gmail.com>
2023-11-13 13:01:45 +0100

    Update authentication file in Controllers to check for user type

--- myschooladmin/app/Http/Controllers/AuthController.php
+++ myschooladmin/app/Http/Controllers/AuthController.php
@@ -13,7 +13,23 @@ class AuthController extends Controller
         //dd(Hash::make(123456));
         if(!empty(Auth::check()))
         {
-            return redirect('admin/dashboard');
+            if(Auth::user()->user_type == 1)
+            {
+                return redirect('admin/dashboard');
+            }
+            else if(Auth::user()->user_type == 2)
+            {
+                return redirect('teacher/dashboard');
+            }
+            else if(Auth::user()->user_type == 3)
+            {
+                return redirect('student/dashboard');
+            }
+            else if(Auth::user()->user_type == 4)
+            {
+                return redirect('parent/dashboard');
+            }
+            
         }
 
         return view('auth.login');
@@ -25,7 +41,23 @@ class AuthController extends Controller
 
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password], $remember))
         {
-            return redirect('admin/dashboard');
+            if(Auth::user()->user_type == 1)
+            {
+                return redirect('admin/dashboard');
+            }
+            else if(Auth::user()->user_type == 2)
+            {
+                return redirect('teacher/dashboard');
+            }
+            else if(Auth::user()->user_type == 3)
+            {
+                return redirect('student/dashboard');
+            }
+            else if(Auth::user()->user_type == 4)
+            {
+                return redirect('parent/dashboard');
+            }
+            
         }
         else
         {
