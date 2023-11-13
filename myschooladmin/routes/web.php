dedd61ef07760c57225877f6bb0094e0ba75cb50
kanayofirms <kanyeze@gmail.com>
2023-11-13 12:40:40 +0100

    Fix: edit routes for functionality

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -23,11 +23,33 @@ Route::get('/', [AuthController::class, 'login']);
 Route::get('logout', [AuthController::class, 'logout']);
 Route::post('login', [AuthController::class, 'AuthLogin']);
 
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard');
-});
+
 
 
 Route::get('admin/admin/list', function () {
     return view('admin.admin.list');
+});
+
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+}); 
+
+Route::group(['middleware' => 'teacher'], function () {
+    Route::get('teacher/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'student'], function () {
+    Route::get('student/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+Route::group(['middleware' => 'parent'], function () {
+    Route::get('parent/dashboard', function () {
+        return view('admin.dashboard');
+    });
 });
