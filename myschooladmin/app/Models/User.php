0dc5554414942a1f2ad7679333445a03b843c0de
kanayofirms <kanyeze@gmail.com>
2023-11-28 05:53:15 +0100

    Model email object for the database

--- myschooladmin/app/Models/User.php
+++ myschooladmin/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    static public function getEmailSingle($email)
+    {
+        return User::where('email', '=', $email)->first();
+    }
 }
