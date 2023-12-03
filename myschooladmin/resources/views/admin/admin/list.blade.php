9b434557932c6f7a36d8e51a83b424b780585a7f
kanayofirms <kanyeze@gmail.com>
2023-12-03 02:22:45 +0100

    Include output message after submitting request

--- myschooladmin/resources/views/admin/admin/list.blade.php
+++ myschooladmin/resources/views/admin/admin/list.blade.php
@@ -33,12 +33,14 @@
           
           <!-- /.col -->
           <div class="col-md-12">
-           
+
+            @include('_message')
+
             <!-- /.card -->
 
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Striped Full Width Table</h3>
+                <h3 class="card-title">Admin List</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body p-0">
