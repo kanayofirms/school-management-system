774123e3187c830cfcf2580173b0faf71024323b
kanayofirms <kanyeze@gmail.com>
2023-12-04 22:06:00 +0100

    Create POST route for edit and update for admin list

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -35,7 +35,10 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('admin/dashboard', [DashboardController::class, 'dashboard']); 
     Route::get('admin/admin/list', [AdminController::class, 'list']); 
     Route::get('admin/admin/add', [AdminController::class, 'add']);
-    Route::post('admin/admin/add', [AdminController::class, 'insert']);  
+    Route::post('admin/admin/add', [AdminController::class, 'insert']);
+    Route::get('admin/admin/edit/{id}', [AdminController::class, 'edit']);
+    Route::post('admin/admin/edit/{id}', [AdminController::class, 'update']);
+      
 }); 
 
 Route::group(['middleware' => 'teacher'], function () {
