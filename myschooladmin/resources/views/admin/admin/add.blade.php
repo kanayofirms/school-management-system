c1ffb207cfdf1fbd0de9dbcb3ff35a48c42c7560
kanayofirms <kanyeze@gmail.com>
2023-12-08 09:51:40 +0100

    Edit: email notification style and include value of old email

--- myschooladmin/resources/views/admin/admin/add.blade.php
+++ myschooladmin/resources/views/admin/admin/add.blade.php
@@ -27,11 +27,14 @@
                 <div class="card-body">
                     <div class="form-group">
                         <label>Name</label>
-                        <input type="text" class="form-control" name="name" required placeholder="Name">
+                        <input type="text" class="form-control" value="{{ old('name') }}" name="name" required placeholder="Name">
                       </div>
                   <div class="form-group">
                     <label>Email</label>
-                    <input type="email" class="form-control" name="email" required placeholder="Email">
+                    <input type="email" class="form-control" value="{{ old('email') }}" name="email" required placeholder="Email">
+                    <div style="color:red">
+                      {{ $errors->first('email') }}
+                    </div>
                   </div>
                   <div class="form-group">
                     <label>Password</label>
