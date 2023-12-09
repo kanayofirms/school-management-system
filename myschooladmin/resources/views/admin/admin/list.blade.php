7dd6ac0d49364b46aa42db8e66ac31923cc3f16e
kanayofirms <kanyeze@gmail.com>
2023-12-09 19:49:28 +0100

    Create query field for admin list and add search button

--- myschooladmin/resources/views/admin/admin/list.blade.php
+++ myschooladmin/resources/views/admin/admin/list.blade.php
@@ -16,23 +16,48 @@
             <a href="{{ url('admin/admin/add') }}" class="btn btn-primary">Add New admin</a>
             
           </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Simple Tables</li>
-            </ol>
-          </div>
-        </div>
+        
       </div><!-- /.container-fluid -->
     </section>
 
     <!-- Main content -->
     <section class="content">
+
       <div class="container-fluid">
         <div class="row">
           
           <!-- /.col -->
           <div class="col-md-12">
+
+            
+                <div class="card"> 
+                  <div class="card-header">
+                    <h3 class="card-title">Search Admin</h3>
+                  </div>  
+                  <form method="get" action="">     
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="form-group col-md-3">
+                            <label>Name</label>
+                            <input type="text" class="form-control" value="" name="name" required placeholder="Name">
+                          </div>
+                          
+                      <div class="form-group col-md-3">
+                        <label>Email</label>
+                        <input type="email" class="form-control" value="" name="email" required placeholder="Email">  
+                      </div>
+
+                      <div class="form-group col-md-3">
+                        <button class="btn btn-primary" type="submit" style="margin-top: 30px;">Search</button>  
+                      </div>
+                      
+                    </div>
+                    </div>
+    
+                  </form>
+                </div>
+                
+    
 
             @include('_message')
 
@@ -84,6 +109,8 @@
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </section>
+
+    
     <!-- /.content -->
   </div>
 
