9da12359a4a3d6aac2bd4e5750754bba446e0bcf
kanayofirms <kanyeze@gmail.com>
2023-12-10 10:22:50 +0100

    Create GET route api for class list

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ClassController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,7 +40,11 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('admin/admin/edit/{id}', [AdminController::class, 'edit']);
     Route::post('admin/admin/edit/{id}', [AdminController::class, 'update']);
     Route::get('admin/admin/delete/{id}', [AdminController::class, 'delete']);
-      
+
+    // class url
+
+    Route::get('admin/class/list', [ClassController::class, 'list']);
+
 }); 
 
 Route::group(['middleware' => 'teacher'], function () {
