18d1d818e56458427d7f75a1e86429fa50605fcd
kanayofirms <kanyeze@gmail.com>
2023-12-11 10:28:29 +0100

    Create insert function to save and send request to the database

--- myschooladmin/app/Http/Controllers/ClassController.php
+++ myschooladmin/app/Http/Controllers/ClassController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\ClassModel;
 
 class ClassController extends Controller
 {
@@ -17,5 +18,16 @@ class ClassController extends Controller
     {
         $data['header_title'] = "Add New Class";
         return view('admin.class.add', $data);
+    }
+
+    public function insert(Request $request)
+    {
+        $save = new ClassModel;
+        $save->name = $request->name;
+        $save->status = $request->status;
+        $save->created_by = Auth::user()->id;
+        $save->save();
+        
+        return redirect('admin/class/list')->with('success', "Class Successfully Created");
     }
 }
