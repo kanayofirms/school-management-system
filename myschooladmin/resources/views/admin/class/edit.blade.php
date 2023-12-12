4da750d6aaf5a8ed52e9adb1963c1b7e585e8f69
kanayofirms <kanyeze@gmail.com>
2023-12-12 20:39:58 +0100

    Update to display value for feasibility

--- myschooladmin/resources/views/admin/class/edit.blade.php
+++ myschooladmin/resources/views/admin/class/edit.blade.php
@@ -27,14 +27,14 @@
                 <div class="card-body">
                     <div class="form-group">
                         <label>Class Name</label>
-                        <input type="text" class="form-control" name="name" required placeholder="Class Name">
+                        <input type="text" class="form-control" value="{{ $getRecord->name }}" name="name" required placeholder="Class Name">
                       </div>
 
                       <div class="form-group">
                         <label>Status</label>
                         <select class="form-control" name="status">
-                            <option value="0">Active</option>
-                            <option value="1">Inactive</option>
+                            <option {{ ($getRecord->status == 0) ? 'selected' : '' }} value="0">Active</option>
+                            <option {{ ($getRecord->status == 1) ? 'selected' : '' }} value="1">Inactive</option>
                         </select>
             
                       </div>
