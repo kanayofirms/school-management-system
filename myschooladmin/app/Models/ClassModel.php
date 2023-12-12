2eecfc7d8ccd4edaedcfe4329e7300626515e192
kanayofirms <kanyeze@gmail.com>
2023-12-12 21:48:22 +0100

    Update class list with filter

--- myschooladmin/app/Models/ClassModel.php
+++ myschooladmin/app/Models/ClassModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class ClassModel extends Model
 {
@@ -19,8 +20,20 @@ class ClassModel extends Model
     static public function getRecord()
     {
         $return = ClassModel::select('class.*', 'users.name as created_by_name')
-                    ->join('users', 'users.id', 'class.created_by')
-                    ->where('class.is_delete', '=', 0)
+                    ->join('users', 'users.id', 'class.created_by');
+
+                    if(!empty(Request::get('name')))
+                    {
+                        $return = $return->where('class.name', 'like', '%'.Request::get('name').'%');
+                    }
+
+                    if(!empty(Request::get('date')))
+                    {
+                        $return = $return->whereDate('class.created_at','=', Request::get('date'));
+                    }
+
+
+                    $return = $return->where('class.is_delete', '=', 0)
                     ->orderBy('class.id', 'desc')
                     ->paginate(20);
 
