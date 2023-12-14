5c062d8c4ecc8b663cd75a3827b73d831e5ee0b4
kanayofirms <kanyeze@gmail.com>
2023-12-14 21:40:05 +0100

    Create GET request route to redirect to assign subject page

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClassController;
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\ClassSubjectController;
 
 
 /*
@@ -60,6 +61,10 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('admin/subject/edit/{id}', [SubjectController::class, 'edit']);
     Route::post('admin/subject/edit/{id}', [SubjectController::class, 'update']);
     Route::get('admin/subject/delete/{id}', [SubjectController::class, 'delete']);
+
+    // assign_subject
+
+    Route::get('admin/assign_subject/list', [ClassSubjectController::class, 'list']);
 
 }); 
 
