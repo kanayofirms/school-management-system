ba064a01ea1a3b7fd8deeb0703362db2f49ed0a6
kanayofirms <kanyeze@gmail.com>
2023-12-14 21:38:45 +0100

    Create controller functions for class subject

---
+++ myschooladmin/app/Http/Controllers/ClassSubjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ClassSubjectModel;
+use App\Models\ClassModel;
+use App\Models\SubjectModel;
+use Auth;
+
+
+class ClassSubjectController extends Controller
+{
+    public function list(Request $request)
+    {
+        $data['getRecord'] = ClassSubjectModel::getRecord();
+
+        $data['header_title'] = "Assign Subject List";
+        return view('admin.assign_subject.list', $data);
+    }
+}
