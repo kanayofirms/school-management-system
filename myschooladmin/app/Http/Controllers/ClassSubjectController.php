b24f3806aabe3767809b05564d3b93838e59228b
kanayofirms <kanyeze@gmail.com>
2023-12-16 07:58:09 +0100

    Fix: redirect to insert all data

--- myschooladmin/app/Http/Controllers/ClassSubjectController.php
+++ myschooladmin/app/Http/Controllers/ClassSubjectController.php
@@ -26,4 +26,25 @@ class ClassSubjectController extends Controller
         $data['header_title'] = "Assign Subject Add";
         return view('admin.assign_subject.add', $data);
     }
+
+    public function insert(Request $request)
+    {
+        if(!empty($request->subject_id))
+        {
+            foreach ($request->subject_id as $subject_id) 
+            {
+                $save = new ClassSubjectModel;
+                $save->class_id = $request->class_id;
+                $save->subject_id = $subject_id;
+                $save->status = $request->status;
+                $save->created_by = Auth::user()->id;
+                $save->save();
+            }
+            return redirect('admin/assign_subject/list')->with('success', "Subject Successfully Assigned to Class");
+        }
+        else
+        {
+            return redirect()->back()->with('error', "Due to some error please try again");
+        }
+    }
 }
