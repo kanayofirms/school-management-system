3f9916ee8f9e59e9c60a5df79a7de7a6bf6e414a
kanayofirms <kanyeze@gmail.com>
2023-12-17 02:49:11 +0100

    Add pagination to the getRecord return function

--- myschooladmin/app/Models/ClassSubjectModel.php
+++ myschooladmin/app/Models/ClassSubjectModel.php
@@ -12,14 +12,38 @@ class ClassSubjectModel extends Model
 
     protected $table = 'class_subject';
 
+    static public function getSingle($id)
+    {
+        return self::find($id);
+    }
+
     static public function getRecord()
     {
-        return self::select('class_subject.*', 'class.name as class_name', 'subject.name as subject_name', 'users.name as created_by_name')
+        $return = self::select('class_subject.*', 'class.name as class_name', 'subject.name as subject_name', 'users.name as created_by_name')
                     ->join('subject', 'subject.id', '=', 'class_subject.subject_id')
                     ->join('class', 'class.id', '=', 'class_subject.class_id')
-                    ->join('users', 'users.id', '=', 'class_subject.created_by')
-                    ->orderBy('class_subject.id', 'desc')
-                    ->paginate(20);
+                    ->join('users', 'users.id', '=', 'class_subject.created_by');
+
+                    if(!empty(Request::get('class_name')))
+                    {
+                        $return = $return->where('class.name', 'like', '%'.Request::get('class_name').'%');
+                    }
+
+                    
+                    if(!empty(Request::get('subject_name')))
+                    {
+                        $return = $return->where('subject.name', 'like', '%'.Request::get('subject_name').'%');
+                    }
+
+                    if(!empty(Request::get('date')))
+                    {
+                        $return = $return->whereDate('class_subject.created_at', '=', Request::get('date'));
+                    }
+
+        $return = $return->orderBy('class_subject.id', 'desc')
+                    ->paginate(100);
+
+        return $return;
     }
 
     static public function getAlreadyFirst($class_id, $subject_id)
