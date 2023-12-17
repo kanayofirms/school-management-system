64dd43c6d6dc89fb6645749bb7a36c23ffbf57b8
kanayofirms <kanyeze@gmail.com>
2023-12-17 08:40:06 +0100

    Fix: change submit button to update

--- myschooladmin/resources/views/admin/assign_subject/edit.blade.php
+++ myschooladmin/resources/views/admin/assign_subject/edit.blade.php
@@ -72,7 +72,7 @@
                                 <!-- /.card-body -->
 
                                 <div class="card-footer">
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="submit" class="btn btn-primary">Update</button>
                                 </div>
                             </form>
                         </div>
