cea86aed71fb44a8fd61a87abe923a4c969a9fdf
kanayofirms <kanyeze@gmail.com>
2023-12-17 12:51:19 +0100

    Edit and update code for feasibility

--- myschooladmin/resources/views/admin/assign_subject/list.blade.php
+++ myschooladmin/resources/views/admin/assign_subject/list.blade.php
@@ -109,6 +109,8 @@
                                                 <td>
                                                     <a href="{{ url('admin/assign_subject/edit/' . $value->id) }}"
                                                         class="btn btn-primary">Edit</a>
+                                                    <a href="{{ url('admin/assign_subject/edit_single/' . $value->id) }}"
+                                                        class="btn btn-primary">Edit Single</a>
                                                     <a href="{{ url('admin/assign_subject/delete/' . $value->id) }}"
                                                         class="btn btn-danger">Delete</a>
                                                 </td>
