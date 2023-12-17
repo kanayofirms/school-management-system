5aee1cf9efbf6e95a9ca4dbfcba23e6bc6ee0df5
kanayofirms <kanyeze@gmail.com>
2023-12-17 12:50:20 +0100

    Create GET route to edit single assign subject

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -69,7 +69,10 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('admin/assign_subject/add', [ClassSubjectController::class, 'insert']);
     Route::get('admin/assign_subject/edit/{id}', [ClassSubjectController::class, 'edit']);
     Route::post('admin/assign_subject/edit/{id}', [ClassSubjectController::class, 'update']);
+    Route::get('admin/assign_subject/edit_single/{id}', [ClassSubjectController::class, 'edit_single']);
+    Route::post('admin/assign_subject/edit_single/{id}', [ClassSubjectController::class, 'update_single']);
     Route::get('admin/assign_subject/delete/{id}', [ClassSubjectController::class, 'delete']);
+
 
 }); 
 
