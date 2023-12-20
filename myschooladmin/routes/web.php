88a527b8b1a471d738f378c9332b33bd2b6bcf0c
kanayofirms <kanyeze@gmail.com>
2023-12-21 00:08:48 +0100

    Create GET and POST routes for change password

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClassController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\ClassSubjectController;
+use App\Http\Controllers\UserController;
+
 
 
 /*
@@ -73,17 +75,34 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('admin/assign_subject/edit_single/{id}', [ClassSubjectController::class, 'update_single']);
     Route::get('admin/assign_subject/delete/{id}', [ClassSubjectController::class, 'delete']);
 
+    // change_password
+
+    Route::get('admin/change_password', [UserController::class, 'change_password']);
+    Route::post('admin/change_password', [UserController::class, 'change_password']);
+
 
 }); 
 
 Route::group(['middleware' => 'teacher'], function () {
     Route::get('teacher/dashboard', [DashboardController::class, 'dashboard']);  
+
+    Route::get('teacher/change_password', [UserController::class, 'change_password']);
+    Route::post('teacher/change_password', [UserController::class, 'change_password']);
+
 });
 
 Route::group(['middleware' => 'student'], function () {
-    Route::get('student/dashboard', [DashboardController::class, 'dashboard']);  
+    Route::get('student/dashboard', [DashboardController::class, 'dashboard']); 
+    
+    Route::get('student/change_password', [UserController::class, 'change_password']);
+    Route::post('student/change_password', [UserController::class, 'change_password']);
+
 });
 
 Route::group(['middleware' => 'parent'], function () {
     Route::get('parent/dashboard', [DashboardController::class, 'dashboard']);  
+
+    Route::get('parent/change_password', [UserController::class, 'change_password']);
+    Route::post('parent/change_password', [UserController::class, 'change_password']);
+
 });
