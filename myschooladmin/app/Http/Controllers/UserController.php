2dcb07e3cee013ba08315dd4567748a3189716e4
kanayofirms <kanyeze@gmail.com>
2023-12-21 02:30:14 +0100

    Create update change password function

--- myschooladmin/app/Http/Controllers/UserController.php
+++ myschooladmin/app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Hash;
 
 class UserController extends Controller
 {
@@ -10,5 +13,21 @@ class UserController extends Controller
     {
         $data['header_title'] = "Change Password";
         return view('profile.change_password', $data);
+    }
+
+    public function update_change_password(Request $request)
+    {
+        $user = User::getSingle(Auth::user()->id);
+        if(Hash::check($request->old_password, $user->password))
+        {
+            $user->password = Hash::make($request->new_password);
+            $user->save();
+
+            return redirect()->back()->with('success', "Password successfully updated");
+        }
+        else
+        {
+            return redirect()->back()->with('error', "Old Password is not correct");
+        }
     }
 }
