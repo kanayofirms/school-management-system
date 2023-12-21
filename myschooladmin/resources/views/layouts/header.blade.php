22e34bf6ebb1504d8c719f63567dfac54c56dcc0
kanayofirms <kanyeze@gmail.com>
2023-12-21 16:46:22 +0100

    Create dashboard menu for student

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -169,6 +169,15 @@
           </li>
 
           <li class="nav-item">
+            <a href="{{ url('admin/student/list') }}" class="nav-link @if(Request::segment(2) == 'student') active @endif">
+              <i class="nav-icon far fa-user"></i>
+              <p>
+                Student
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
             <a href="{{ url('admin/class/list') }}" class="nav-link @if(Request::segment(2) == 'class') active @endif">
               <i class="nav-icon far fa-user"></i>
               <p>
