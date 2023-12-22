1c59554b156e9692829ef92091b01b71a72ad350
kanayofirms <kanyeze@gmail.com>
2023-12-22 05:07:44 +0100

    Create more input fields and update add student page

--- myschooladmin/resources/views/admin/student/add.blade.php
+++ myschooladmin/resources/views/admin/student/add.blade.php
@@ -55,10 +55,109 @@
                                                 name="roll_number" placeholder="Roll Number">
                                         </div>
 
+                                        <div class="form-group col-md-6">
+                                            <label>Class <span style="color: red;">*</span></label>
+                                            <select name="form-control" required name="class_id">
+                                                <option value="">Select Class</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Gender <span style="color: red;">*</span></label>
+                                            <select name="form-control" required name="gender">
+                                                <option value="">Select Gender</option>
+                                                <option value="Male">Male</option>
+                                                <option value="Female">Female</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Date of Birth <span style="color: red;">*</span></label>
+                                            <input type="date" class="form-control" required value="{{ old('date_of_birth') }}"
+                                                name="date_of_birth">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Address <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" value="{{ old('address') }}"
+                                                name="address" required placeholder="Address">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Country <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" value="{{ old('country') }}"
+                                                name="country" required placeholder="Country">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>State <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" value="{{ old('state') }}"
+                                                name="state" required placeholder="State">
+                                        </div>
+
+                                        
+                                        <div class="form-group col-md-6">
+                                            <label>Local Government Area <span style="color: red;">*</span></label>
+                                            <input type="text" class="form-control" value="{{ old('local_govt_area') }}"
+                                                name="local_govt_area" required placeholder="Local Governmet Area">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Hometown <span style="color: red;"></span></label>
+                                            <input type="text" class="form-control" value="{{ old('hometown') }}"
+                                                name="hometown" placeholder="Hometown">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Religion <span style="color: red;"></span></label>
+                                            <input type="text" class="form-control" value="{{ old('religion') }}"
+                                                name="religion" placeholder="Religion">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Admission Date <span style="color: red;">*</span></label>
+                                            <input type="date" class="form-control" required value="{{ old('admission_date') }}"
+                                                name="admission_date">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Profile Picture <span style="color: red;"></span></label>
+                                            <input type="file" class="form-control" name="profile_pic">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Blood Group <span style="color: red;"></span></label>
+                                            <input type="text" class="form-control" value="{{ old('blood_group') }}"
+                                                name="blood_group" placeholder="Blood Group">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Genotype <span style="color: red;"></span></label>
+                                            <input type="text" class="form-control" value="{{ old('genotype') }}"
+                                                name="genotype" placeholder="Genotype">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Disability <span style="color: red;"></span></label>
+                                            <input type="text" class="form-control" value="{{ old('disability') }}"
+                                                name="disability" placeholder="Disability">
+                                        </div>
+
+                                        <div class="form-group col-md-6">
+                                            <label>Status <span style="color: red;">*</span></label>
+                                            <select name="form-control" required name="status">
+                                                <option value="">Select Status</option>
+                                                <option value="0">Active</option>
+                                                <option value="1">Inactive</option>
+                                            </select>
+                                        </div>
+
                                     </div>
+
+                                    <hr />
                                   
                                     <div class="form-group">
-                                        <label>Email</label>
+                                        <label> <span style="color: red;"></span></label>
                                         <input type="email" class="form-control" value="{{ old('email') }}"
                                             name="email" required placeholder="Email">
                                         <div style="color:red">
@@ -66,7 +165,7 @@
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <label>Password</label>
+                                        <label>Password <span style="color: red;"></span></label>
                                         <input type="password" class="form-control" name="password" required
                                             placeholder="Password">
                                     </div>
