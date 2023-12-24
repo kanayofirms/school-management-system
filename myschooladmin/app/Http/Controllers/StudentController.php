1ec3492b749145e91eba6ed135aec61c1634e45b
kanayofirms <kanyeze@gmail.com>
2023-12-24 17:38:16 +0100

    Create function to save profile picture

--- myschooladmin/app/Http/Controllers/StudentController.php
+++ myschooladmin/app/Http/Controllers/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\ClassModel;
+use Illuminate\Support\Str;
 use Hash;
 use Auth;
 
@@ -38,19 +39,36 @@ class StudentController extends Controller
         {
             $student->date_of_birth = trim($request->date_of_birth);
         }
+
+        if(!empty($request->file('profile_pic')))
+        {
+            $ext = $request->file('profile_pic')->getClientOriginalExtension();
+            $file = $request->file('profile_pic');
+            $randomStr = date('Ymdhis').Str::random(20);
+            $filename = strtolower($randomStr).'.'.$ext;
+            $file->move('upload/profile/', $filename);
+
+            $student->profile_pic = $filename;
+        }
         $student->address = trim($request->address);
         $student->country = trim($request->country);
         $student->state = trim($request->state);
         $student->local_govt_area = trim($request->local_govt_area);
         $student->hometown = trim($request->hometown);
         $student->religion = trim($request->religion);
-        $student->admission_date = trim($request->admission_date);
+
+        if(!empty($request->admission_date))
+        {
+            $student->admission_date = trim($request->admission_date);
+        }
+        
         $student->blood_group = trim($request->blood_group);
         $student->genotype = trim($request->genotype);
         $student->disability = trim($request->disability);
         $student->status = trim($request->status);
         $student->email = trim($request->email);
         $student->password = Hash::make($request->password);
+        $student->user_type = 3;
         $student->save();
 
         return redirect('admin/student/list')->with('success', "Student Successfully Created");
