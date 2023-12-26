d32c1340dc9d7f3b40f5fc6d9ecdade96cef3cc6
kanayofirms <kanyeze@gmail.com>
2023-12-26 22:07:05 +0100

    Create update function to update student record by id

--- myschooladmin/app/Http/Controllers/StudentController.php
+++ myschooladmin/app/Http/Controllers/StudentController.php
@@ -102,4 +102,71 @@ class StudentController extends Controller
             abort(404);
         }
     }
+
+    public function update($id, Request $request)
+    {
+        request()->validate([
+            'email' => 'required|email|unique:users,email,'.$id,
+            'genotype' => 'max:10',
+            'blood_group' => 'max:10',
+            'religion' => 'max:50',
+            'admission_number' => 'max:50',
+            'roll_number' => 'max:50',
+            'country' => 'max:50',
+            'state' => 'max:50',
+            'local_govt_area' => 'max:50',
+            'hometown' => 'max:50',        
+        ]);
+
+        $student = User::getSingle($id);
+        $student->name = trim($request->last_name);
+        $student->middle_name = trim($request->middle_name);
+        $student->last_name = trim($request->last_name);
+        $student->admission_number = trim($request->admission_number);
+        $student->roll_number = trim($request->roll_number);
+        $student->gender = trim($request->gender);
+        
+        if(!empty($request->date_of_birth))
+        {
+            $student->date_of_birth = trim($request->date_of_birth);
+        }
+
+        if(!empty($request->file('profile_pic')))
+        {
+            $ext = $request->file('profile_pic')->getClientOriginalExtension();
+            $file = $request->file('profile_pic');
+            $randomStr = date('Ymdhis').Str::random(20);
+            $filename = strtolower($randomStr).'.'.$ext;
+            $file->move('upload/profile/', $filename);
+
+            $student->profile_pic = $filename;
+        }
+        $student->address = trim($request->address);
+        $student->country = trim($request->country);
+        $student->state = trim($request->state);
+        $student->local_govt_area = trim($request->local_govt_area);
+        $student->hometown = trim($request->hometown);
+        $student->religion = trim($request->religion);
+
+        if(!empty($request->admission_date))
+        {
+            $student->admission_date = trim($request->admission_date);
+        }
+        
+        $student->blood_group = trim($request->blood_group);
+        $student->genotype = trim($request->genotype);
+        $student->disability = trim($request->disability);
+        $student->status = trim($request->status);
+        $student->email = trim($request->email);
+        if(!empty($request->password))
+        {
+            $student->password = Hash::make($request->password);   
+        }
+        
+
+        $student->save();
+
+        return redirect('admin/student/list')->with('success', "Student Successfully Updated");
+
+    }
 }
