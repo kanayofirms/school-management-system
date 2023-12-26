33a44f08e9e14558f5e60a3171e80dba010f66a5
kanayofirms <kanyeze@gmail.com>
2023-12-26 22:03:39 +0100

    Create POST route to update student record by id

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -43,6 +43,7 @@ Route::get('admin/student/list', [StudentController::class, 'list']);
 Route::get('admin/student/add', [StudentController::class, 'add']);
 Route::post('admin/student/add', [StudentController::class, 'insert']);
 Route::get('admin/student/edit/{id}', [StudentController::class, 'edit']);
+Route::post('admin/student/edit/{id}', [StudentController::class, 'update']);
 
 
 
