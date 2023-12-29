d8270ce3994c3abdc04d46180c04ea1353970d45
kanayofirms <kanyeze@gmail.com>
2023-12-29 23:20:52 +0100

    Refactor and update frontend code for feasibility

--- myschooladmin/resources/views/admin/parent/list.blade.php
+++ myschooladmin/resources/views/admin/parent/list.blade.php
@@ -30,27 +30,69 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Search Parent</h3>
+                                <h3 class="card-title">Search Student</h3>
                             </div>
                             <form method="get" action="">
                                 <div class="card-body">
                                     <div class="row">
-                                        <div class="form-group col-md-3">
+                                        <div class="form-group col-md-2">
                                             <label>Name</label>
                                             <input type="text" class="form-control" value="{{ Request::get('name') }}"
                                                 name="name" placeholder="Name">
                                         </div>
 
-                                        <div class="form-group col-md-3">
+                                        <div class="form-group col-md-2">
+                                            <label>Last Name</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('last_name') }}"
+                                                name="last_name" placeholder="Last Name">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
                                             <label>Email</label>
                                             <input type="text" class="form-control" value="{{ Request::get('email') }}"
                                                 name="email" placeholder="Email">
                                         </div>
 
-                                        <div class="form-group col-md-3">
-                                            <label>Date</label>
+                                        <div class="form-group col-md-2">
+                                            <label>Gender</label>
+                                            <select class="form-control" name="gender">
+                                                <option value="">Select Gender</option>
+                                                <option  {{ (Request::get('gender') == 'Male') ? 'selected' : '' }} value="Male">Male</option>
+                                                <option  {{ (Request::get('gender') == 'Female') ? 'selected' : '' }} value="Female">Female</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Mobile Number</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('mobile_number') }}"
+                                                name="mobile_number" placeholder="Mobile Number">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Address</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('address') }}"
+                                                name="address" placeholder="Address">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Occupation</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('occupation') }}"
+                                                name="occupation" placeholder="Occupation">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Status</label>
+                                            <select class="form-control" name="status">
+                                                <option value="">Select Status</option>
+                                                <option  {{ (Request::get('status') == 100) ? 'selected' : '' }} value="100">Active</option>
+                                                <option  {{ (Request::get('status') == 1) ? 'selected' : '' }} value="1">Inactive</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Created Date</label>
                                             <input type="date" class="form-control" value="{{ Request::get('date') }}"
-                                                name="date" placeholder="Email">
+                                                name="date" placeholder="">
                                         </div>
 
                                         <div class="form-group col-md-3">
@@ -87,8 +129,8 @@
                                             <th>Email</th>
                                             <th>Gender</th>
                                             <th>Mobile Number</th>
-                                            <th>Occupation</th>
                                             <th>Address</th>
+                                            <th>Occupation</th>
                                             <th>Status</th>
                                             <th>Created Date</th>
                                             <th>Action</th>
@@ -107,8 +149,8 @@
                                                 <td>{{ $value->email }}</td>
                                                 <td>{{ $value->gender }}</td>
                                                 <td>{{ $value->mobile_number }}</td>
-                                                <td>{{ $value->occupation }}</td>
                                                 <td>{{ $value->address }}</td>
+                                                <td>{{ $value->occupation }}</td>
                                                 <td>{{ ($value->status == 0) ? 'Active' : 'Inactive' }}</td>
                                                 <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
                                                 <td>
