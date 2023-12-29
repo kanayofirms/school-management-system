fe51b5d83709bb692f345ae607318136dddd9faa
kanayofirms <kanyeze@gmail.com>
2023-12-29 03:56:17 +0100

    Create parent frontend page

---
+++ myschooladmin/resources/views/admin/parent/list.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Parent List (Total: {{ $getRecord->Total() }})</h1>
+                    </div>
+                    <div class="col-sm-6" style="text-align: right;">
+                        <a href="{{ url('admin/parent/add') }}" class="btn btn-primary">Add New Parent</a>
+
+                    </div>
+
+                </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+
+                    <!-- /.col -->
+                    <div class="col-md-12">
+
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Search Parent</h3>
+                            </div>
+                            <form method="get" action="">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-3">
+                                            <label>Name</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('name') }}"
+                                                name="name" placeholder="Name">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label>Email</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('email') }}"
+                                                name="email" placeholder="Email">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label>Date</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('date') }}"
+                                                name="date" placeholder="Email">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <button class="btn btn-primary" type="submit"
+                                                style="margin-top: 30px;">Search</button>
+                                            <a href="{{ url('admin/parent/list') }}" class="btn btn-success"
+                                                style="margin-top: 30px;">Reset</a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+
+
+
+                        @include('_message')
+
+                        <!-- /.card -->
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Parent List</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Profile Pic</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Gender</th>
+                                            <th>Mobile Number</th>
+                                            <th>Occupation</th>
+                                            <th>Address</th>
+                                            <th>Status</th>
+                                            <th>Created Date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($getRecord as $value)
+                                            <tr>
+                                                <td>{{ $value->id }}</td>
+                                                <td>
+                                                    @if (!@empty($value->getProfile()))
+                                                        <img src="{{ $value->getProfile() }}" style="height: 50px; width: 50px; border-radius: 50%;">
+                                                    @endif
+                                                </td>
+                                                <td>{{ $value->name }} {{ $value->last_name }}</td>
+                                                <td>{{ $value->email }}</td>
+                                                <td>{{ $value->gender }}</td>
+                                                <td>{{ $value->mobile_number }}</td>
+                                                <td>{{ $value->occupation }}</td>
+                                                <td>{{ $value->address }}</td>
+                                                <td>{{ ($value->status == 0) ? 'Active' : 'Inactive' }}</td>
+                                                <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
+                                                <td>
+                                                    <a href="{{ url('admin/parent/edit/' . $value->id) }}"
+                                                        class="btn btn-primary">Edit</a>
+                                                    <a href="{{ url('admin/parent/delete/' . $value->id) }}"
+                                                        class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                <div style="padding: 10px; float:right;">
+                                    {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+
+
+        <!-- /.content -->
+    </div>
+@endsection
