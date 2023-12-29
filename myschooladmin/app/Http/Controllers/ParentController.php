7eb6ccc6a57a5856702d78e6c428af951a997a11
kanayofirms <kanyeze@gmail.com>
2023-12-29 18:20:09 +0100

    Update insert function with request validations

--- myschooladmin/app/Http/Controllers/ParentController.php
+++ myschooladmin/app/Http/Controllers/ParentController.php
@@ -22,4 +22,41 @@ class ParentController extends Controller
         $data['header_title'] = "Add New Parent";
         return view('admin.parent.add', $data);
     }
+
+    public function insert(Request $request)
+    {
+        request()->validate([
+            'email' => 'required|email|unique:users',
+            'mobile_number' => 'max:14|min:11',
+            'address' => 'max:255',
+            'occupation' => 'max:255',       
+        ]);
+
+        $student = new User;
+        $student->name = trim($request->name);
+        $student->last_name = trim($request->last_name);
+        $student->gender = trim($request->gender);
+        $student->occupation = trim($request->occupation);
+        
+        if(!empty($request->file('profile_pic')))
+        {
+            $ext = $request->file('profile_pic')->getClientOriginalExtension();
+            $file = $request->file('profile_pic');
+            $randomStr = date('Ymdhis').Str::random(20);
+            $filename = strtolower($randomStr).'.'.$ext;
+            $file->move('upload/profile/', $filename);
+
+            $student->profile_pic = $filename;
+        }
+        $student->address = trim($request->address);
+        $student->mobile_number = trim($request->mobile_number);
+        $student->status = trim($request->status);
+        $student->email = trim($request->email);
+        $student->password = Hash::make($request->password);
+        $student->user_type = 4;
+        $student->save();
+
+        return redirect('admin/parent/list')->with('success', "Parent Successfully Created");
+
+    }
 }
