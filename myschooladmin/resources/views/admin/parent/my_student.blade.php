031260b9ca3037485de586d06ab89049738de91f
kanayofirms <kanyeze@gmail.com>
2023-12-30 02:37:49 +0100

    Create student list table

--- myschooladmin/resources/views/admin/parent/my_student.blade.php
+++ myschooladmin/resources/views/admin/parent/my_student.blade.php
@@ -25,11 +25,18 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Search Student</h3>
+                                <h3 class="card-title">Search Parent</h3>
                             </div>
                             <form method="get" action="">
                                 <div class="card-body">
                                     <div class="row">
+
+                                        <div class="form-group col-md-2">
+                                            <label>Student ID</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('id') }}"
+                                                name="id" placeholder="Student ID">
+                                        </div>
+
                                         <div class="form-group col-md-2">
                                             <label>Name</label>
                                             <input type="text" class="form-control" value="{{ Request::get('name') }}"
@@ -48,52 +55,10 @@
                                                 name="email" placeholder="Email">
                                         </div>
 
-                                        <div class="form-group col-md-2">
-                                            <label>Gender</label>
-                                            <select class="form-control" name="gender">
-                                                <option value="">Select Gender</option>
-                                                <option  {{ (Request::get('gender') == 'Male') ? 'selected' : '' }} value="Male">Male</option>
-                                                <option  {{ (Request::get('gender') == 'Female') ? 'selected' : '' }} value="Female">Female</option>
-                                            </select>
-                                        </div>
-
-                                        <div class="form-group col-md-2">
-                                            <label>Mobile Number</label>
-                                            <input type="text" class="form-control" value="{{ Request::get('mobile_number') }}"
-                                                name="mobile_number" placeholder="Mobile Number">
-                                        </div>
-
-                                        <div class="form-group col-md-2">
-                                            <label>Address</label>
-                                            <input type="text" class="form-control" value="{{ Request::get('address') }}"
-                                                name="address" placeholder="Address">
-                                        </div>
-
-                                        <div class="form-group col-md-2">
-                                            <label>Occupation</label>
-                                            <input type="text" class="form-control" value="{{ Request::get('occupation') }}"
-                                                name="occupation" placeholder="Occupation">
-                                        </div>
-
-                                        <div class="form-group col-md-2">
-                                            <label>Status</label>
-                                            <select class="form-control" name="status">
-                                                <option value="">Select Status</option>
-                                                <option  {{ (Request::get('status') == 100) ? 'selected' : '' }} value="100">Active</option>
-                                                <option  {{ (Request::get('status') == 1) ? 'selected' : '' }} value="1">Inactive</option>
-                                            </select>
-                                        </div>
-
-                                        <div class="form-group col-md-2">
-                                            <label>Created Date</label>
-                                            <input type="date" class="form-control" value="{{ Request::get('date') }}"
-                                                name="date" placeholder="">
-                                        </div>
-
                                         <div class="form-group col-md-3">
                                             <button class="btn btn-primary" type="submit"
                                                 style="margin-top: 30px;">Search</button>
-                                            <a href="{{ url('admin/parent/list') }}" class="btn btn-success"
+                                            <a href="{{ url('admin/parent/my-student/'.$parent_id) }}" class="btn btn-success"
                                                 style="margin-top: 30px;">Reset</a>
                                         </div>
 
@@ -111,7 +76,37 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Parent School List</h3>
+                                <h3 class="card-title">Student List</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Profile Pic</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Created Date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                       
+                                    </tbody>
+                                </table>
+                                <div style="padding: 10px; float:right;">
+        
+                                </div>
+                            </div>
+
+                            
+                            <!-- /.card-body -->
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Parent Student List</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body p-0">
@@ -139,6 +134,8 @@
         
                                 </div>
                             </div>
+
+                            
                             <!-- /.card-body -->
                         </div>
                         <!-- /.card -->
