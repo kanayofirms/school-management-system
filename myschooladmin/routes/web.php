c895a38e244aea5ac9f1ebd96cb7dde3ba42c871
kanayofirms <kanyeze@gmail.com>
2023-12-30 04:43:44 +0100

    Create GET route for assign student to parent

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -57,6 +57,8 @@ Route::get('admin/parent/edit/{id}', [ParentController::class, 'edit']);
 Route::post('admin/parent/edit/{id}', [ParentController::class, 'update']);
 Route::get('admin/parent/delete/{id}', [ParentController::class, 'delete']);
 Route::get('admin/parent/my-student/{id}', [ParentController::class, 'myStudent']);
+Route::get('admin/parent/assign_student_parent/{student_id}/{parent_id}', [ParentController::class, 'assignStudentParent']);
+
 
 
 
