6f9db56da7d083852a9ad37d2e02faed15dfe5b5
kanayofirms <kanyeze@gmail.com>
2023-12-30 19:06:57 +0100

    Create table for parent student list

--- myschooladmin/resources/views/admin/parent/my_student.blade.php
+++ myschooladmin/resources/views/admin/parent/my_student.blade.php
@@ -103,7 +103,7 @@
                                                     <img src="{{ $value->getProfile() }}" style="height: 50px; width: 50px; border-radius: 50%;">    
                                                     @endif
                                                 </td>
-                                                <td>{{ $value->name }} {{ $value->middle_name }} {{ $value->last_name }}</td>
+                                                <td>{{ $value->name }} {{ $value->last_name }}</td>
                                                 <td>{{ $value->email }}</td>
                                                 <td>{{ $value->parent_name }}</td>
                                                 <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
@@ -136,19 +136,33 @@
                                         <tr>
                                             <th>#</th>
                                             <th>Profile Pic</th>
-                                            <th>Name</th>
+                                            <th>Student Name</th>
                                             <th>Email</th>
-                                            <th>Gender</th>
-                                            <th>Mobile Number</th>
-                                            <th>Address</th>
-                                            <th>Occupation</th>
-                                            <th>Status</th>
+                                            <th>Parent Name</th>
                                             <th>Created Date</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                       
+                                        @foreach ($getRecord as $value)
+                                            <tr>
+                                                <td>{{ $value->id }}</td>
+                                                <td>
+                                                    @if (!@empty($value->getProfile()))
+                                                    <img src="{{ $value->getProfile() }}" style="height: 50px; width: 50px; border-radius: 50%;">    
+                                                    @endif
+                                                </td>
+                                                <td>{{ $value->name }} {{ $value->last_name }}</td>
+                                                <td>{{ $value->email }}</td>
+                                                <td>{{ $value->parent_name }}</td>
+                                                <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
+                                                <td style="min-width: 150px;">
+
+                                                    <a href="{{ url('admin/parent/assign_student_parent_delete/'.$value->id) }}"
+                                                        class="btn btn-danger btn-sm">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
                                 <div style="padding: 10px; float:right;">
