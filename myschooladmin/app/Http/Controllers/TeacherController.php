e060da6d3bffc378210e00dc0942c6743881218c
kanayofirms <kanyeze@gmail.com>
2024-01-04 14:33:37 +0100

    Create controller functions for teacher module

---
+++ myschooladmin/app/Http/Controllers/TeacherController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
+use Auth;
+use Str;
+
+class TeacherController extends Controller
+{
+    public function list()
+    {
+        $data['getRecord'] = User::getTeacher();
+        $data['header_title'] = "Teacher List";
+        return view('admin.teacher.list', $data);
+    }
+}
