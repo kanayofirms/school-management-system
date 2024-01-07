cb9e744c9ca272fc37caffeebb8a44679671ff44
kanayofirms <kanyeze@gmail.com>
2024-01-07 01:46:56 +0100

    Refactor code for feasibility

--- myschooladmin/app/Http/Controllers/TeacherController.php
+++ myschooladmin/app/Http/Controllers/TeacherController.php
@@ -22,5 +22,52 @@ class TeacherController extends Controller
         $data['header_title'] = "Add New Teacher";
         return view('admin.teacher.add', $data);
     }
-    
+
+    public function insert(Request $request)
+    {
+        request()->validate([
+            'email' => 'required|email|unique:users',
+            'mobile_number' => 'max:14|min:11',
+            'marital_status' => 'max:50',
+        ]);
+
+        $teacher = new User;
+        $teacher->name = trim($request->name);
+        $teacher->last_name = trim($request->last_name);
+        $teacher->gender = trim($request->gender);
+
+        if(!empty($request->date_of_birth))
+        {
+            $teacher->date_of_birth = trim($request->date_of_birth);
+        }
+        if(!empty($request->admission_date))
+        {
+            $teacher->admission_date = trim($request->admission_date);
+        }
+        if(!empty($request->file('profile_pic')))
+        {
+            $ext = $request->file('profile_pic')->getClientOriginalExtension();
+            $file = $request->file('profile_pic');
+            $randomStr = date('Ymdhis').Str::random(20);
+            $filename = strtolower($randomStr).'.'.$ext;
+            $file->move('upload/profile/', $filename);
+
+            $teacher->profile_pic = $filename;
+        }
+
+        $teacher->marital_status = trim($request->marital_status);
+        $teacher->address = trim($request->address);
+        $teacher->mobile_number = trim($request->mobile_number);
+        $teacher->qualification = trim($request->qualification);
+        $teacher->note = trim($request->note);
+        $teacher->status = trim($request->status);
+        $teacher->email = trim($request->email);
+        $teacher->password = Hash::make($request->password);
+        $teacher->user_type = 2;
+        $teacher->save();
+
+        return redirect('admin/teacher/list')->with('success', "Teacher Successfully Created");
+
+    }
+
 }
