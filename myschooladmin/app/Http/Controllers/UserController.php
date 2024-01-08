11d4e062aef46b281b90870d2c42be8ed8087416
kanayofirms <kanyeze@gmail.com>
2024-01-08 01:00:57 +0100

    Create updateMyAccount function to update teacher account

--- myschooladmin/app/Http/Controllers/UserController.php
+++ myschooladmin/app/Http/Controllers/UserController.php
@@ -16,6 +16,49 @@ class UserController extends Controller
         return view('teacher.my_account', $data);
     }
 
+    public function updateMyAccount(Request $request)
+    {
+        $id = Auth::user()->id;
+        request()->validate([
+            'email' => 'required|email|unique:users,email,'.$id,
+            'mobile_number' => 'max:14|min:11',
+            'marital_status' => 'max:50',
+        ]);
+
+        $teacher = User::getSingle($id);
+        $teacher->name = trim($request->name);
+        $teacher->last_name = trim($request->last_name);
+        $teacher->gender = trim($request->gender);
+
+        if(!empty($request->date_of_birth))
+        {
+            $teacher->date_of_birth = trim($request->date_of_birth);
+        }
+    
+        if(!empty($request->file('profile_pic')))
+        {
+            $ext = $request->file('profile_pic')->getClientOriginalExtension();
+            $file = $request->file('profile_pic');
+            $randomStr = date('Ymdhis').Str::random(20);
+            $filename = strtolower($randomStr).'.'.$ext;
+            $file->move('upload/profile/', $filename);
+
+            $teacher->profile_pic = $filename;
+        }
+
+        $teacher->marital_status = trim($request->marital_status);
+        $teacher->address = trim($request->address);
+        $teacher->mobile_number = trim($request->mobile_number);
+        $teacher->qualification = trim($request->qualification);
+        
+        $teacher->email = trim($request->email);
+        
+        
+        $teacher->save();
+
+        return redirect()->back()->with('success', "Account Successfully Updated");
+    }
+
     public function change_password()
     {
         $data['header_title'] = "Change Password";
