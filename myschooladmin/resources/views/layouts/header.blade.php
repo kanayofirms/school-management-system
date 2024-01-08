3ecbc9cec02f0cbf746a0b491538cdf7f0a4b977
kanayofirms <kanyeze@gmail.com>
2024-01-08 16:56:17 +0100

    Create dashboard menu for admin account

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -223,6 +223,15 @@
           </li>
 
           <li class="nav-item">
+            <a href="{{ url('admin/account') }}" class="nav-link @if(Request::segment(2) == 'account') active @endif">
+              <i class="nav-icon far fa-user"></i>
+              <p>
+                My Account
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
             <a href="{{ url('admin/change_password') }}" class="nav-link @if(Request::segment(2) == 'change_password') active @endif">
               <i class="nav-icon far fa-user"></i>
               <p>
