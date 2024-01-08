5fd538f587ede2890104587266d7605c760f871b
kanayofirms <kanyeze@gmail.com>
2024-01-08 16:12:13 +0100

    Create updateMyAccountParent function to update parent account

--- myschooladmin/app/Http/Controllers/UserController.php
+++ myschooladmin/app/Http/Controllers/UserController.php
@@ -127,6 +127,47 @@ class UserController extends Controller
         return redirect()->back()->with('success', "Account Successfully Updated");
     }
 
+    public function updateMyAccountParent(Request $request)
+    {
+        $id = Auth::user()->id;
+        request()->validate([
+            'email' => 'required|email|unique:users,email,'.$id,
+            'mobile_number' => 'max:14|min:11',
+            'address' => 'max:255',
+            'occupation' => 'max:255',         
+        ]);
+
+        $parent = User::getSingle($id);
+
+        $parent->name = trim($request->name);
+        $parent->last_name = trim($request->last_name);
+        $parent->gender = trim($request->gender);
+        $parent->occupation = trim($request->occupation);
+        
+        if(!empty($request->file('profile_pic')))
+        {
+            if(!empty($student->getProfile()))
+            {
+                unlink('upload/profile/'.$parent->profile_pic);
+            }
+
+            $ext = $request->file('profile_pic')->getClientOriginalExtension();
+            $file = $request->file('profile_pic');
+            $randomStr = date('Ymdhis').Str::random(20);
+            $filename = strtolower($randomStr).'.'.$ext;
+            $file->move('upload/profile/', $filename);
+
+            $parent->profile_pic = $filename;
+        }
+        $parent->address = trim($request->address);
+        $parent->mobile_number = trim($request->mobile_number);
+        $parent->email = trim($request->email);
+        
+        $parent->save();
+
+        return redirect()->back()->with('success', "Account Successfully Updated");
+    }
+
     public function change_password()
     {
         $data['header_title'] = "Change Password";
