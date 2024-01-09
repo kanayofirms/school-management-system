1e472cc43f4d8bfa0fb00b24527e3e02ea00daf8
kanayofirms <kanyeze@gmail.com>
2024-01-09 21:27:17 +0100

    Edit: remove id from list

--- myschooladmin/resources/views/parent/my_student.blade.php
+++ myschooladmin/resources/views/parent/my_student.blade.php
@@ -35,28 +35,66 @@
                                 <h3 class="card-title">My Student</h3>
                             </div>
                             <!-- /.card-header -->
-                            <div class="card-body p-0">
+                            <div class="card-body p-0" style="overflow: auto;">
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
-                                            <th>#</th>
+                                            
                                             <th>Profile Pic</th>
                                             <th>Student Name</th>
                                             <th>Email</th>
+                                            <th>Admission No.</th>
+                                            <th>Roll No.</th>
+                                            <th>Admission Date</th>
+                                            <th>Class</th>
+                                            <th>Gender</th>
+                                            <th>Date of Birth</th>
+                                            <th>Address</th>
+                                            <th>Country</th>
+                                            <th>State</th>
+                                            <th>Local Government Area</th>
+                                            <th>Hometown</th>
+                                            <th>Religion</th>
+                                            <th>Blood Group</th>
+                                            <th>Genotype</th>
+                                            <th>Disability</th>
                                             <th>Created Date</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($getRecord as $value)
                                             <tr>
-                                                <td>{{ $value->id }}</td>
+                                               
                                                 <td>
                                                     @if (!@empty($value->getProfile()))
                                                     <img src="{{ $value->getProfile() }}" style="height: 50px; width: 50px; border-radius: 50%;">    
                                                     @endif
                                                 </td>
-                                                <td>{{ $value->name }} {{ $value->last_name }}</td>
+                                                <td>{{ $value->name }} {{ $value->middle_name }} {{ $value->last_name }}</td>
                                                 <td>{{ $value->email }}</td>
+                                                <td>{{ $value->admission_number }}</td>
+                                                <td>{{ $value->roll_number }}</td>
+                                                <td>
+                                                    @if (!@empty($value->admission_date))
+                                                        {{ date('d-m-Y', strtotime($value->admission_date)) }}
+                                                    @endif
+                                                </td>
+                                                <td>{{ $value->class_name }}</td>
+                                                <td>{{ $value->gender }}</td>
+                                                <td>
+                                                    @if (!@empty($value->date_of_birth))
+                                                        {{ date('d-m-Y', strtotime($value->date_of_birth)) }}
+                                                    @endif
+                                                </td>
+                                                <td>{{ $value->address }}</td>
+                                                <td>{{ $value->country }}</td>
+                                                <td>{{ $value->state }}</td>
+                                                <td>{{ $value->local_govt_area }}</td>
+                                                <td>{{ $value->hometown }}</td>
+                                                <td>{{ $value->religion }}</td>
+                                                <td>{{ $value->blood_group }}</td>
+                                                <td>{{ $value->genotype }}</td>
+                                                <td>{{ $value->disability }}</td>
                                                 <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
                                             </tr>
                                         @endforeach
