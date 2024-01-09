3ffdca96d7722cf64c5cd9514352b3692ddcb5bc
kanayofirms <kanyeze@gmail.com>
2024-01-10 00:43:04 +0100

    Create frontend page for student subject

---
+++ myschooladmin/resources/views/student/my_subject.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+      <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>My Subject</h1>
+          </div>
+         
+
+        
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <div class="container-fluid">
+        <div class="row">
+          
+          <!-- /.col -->
+          <div class="col-md-12">
+
+            
+               
+                
+    
+
+            @include('_message')
+
+            <!-- /.card -->
+
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">My Subject</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th>Subject Name</th>
+                      <th>Subject Type</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                        
+                  </tbody>
+                </table>
+                
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+        
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+
+    
+    <!-- /.content -->
+  </div>
+
+@endsection
