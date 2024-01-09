3f3cf6aeb5a5613ac3aa0d9d8ab7f8ae98ade254
kanayofirms <kanyeze@gmail.com>
2024-01-10 00:41:35 +0100

    Create dashboard menu for student subject

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -283,6 +283,15 @@
         </li>
 
         <li class="nav-item">
+          <a href="{{ url('student/my_subject') }}" class="nav-link @if(Request::segment(2) == 'my_subject') active @endif">
+            <i class="nav-icon far fa-user"></i>
+            <p>
+              My Subject
+            </p>
+          </a>
+        </li>
+
+        <li class="nav-item">
           <a href="{{ url('student/account') }}" class="nav-link @if(Request::segment(2) == 'account') active @endif">
             <i class="nav-icon far fa-user"></i>
             <p>
