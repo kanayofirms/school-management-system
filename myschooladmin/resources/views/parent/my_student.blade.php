86c7cb37b78f19f93c1951b00084e9ee4798f31d
kanayofirms <kanyeze@gmail.com>
2024-01-09 20:12:49 +0100

    Create frontend page for my student parent account

---
+++ myschooladmin/resources/views/parent/my_student.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>My Student</h1>
+                    </div>
+                </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+
+                    <!-- /.col -->
+                    <div class="col-md-12">
+
+
+
+
+                        @include('_message')
+
+                        <!-- /.card -->
+
+ 
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">My Student</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Profile Pic</th>
+                                            <th>Student Name</th>
+                                            <th>Email</th>
+                                            <th>Parent Name</th>
+                                            <th>Created Date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($getRecord as $value)
+                                            <tr>
+                                                <td>{{ $value->id }}</td>
+                                                <td>
+                                                    @if (!@empty($value->getProfile()))
+                                                    <img src="{{ $value->getProfile() }}" style="height: 50px; width: 50px; border-radius: 50%;">    
+                                                    @endif
+                                                </td>
+                                                <td>{{ $value->name }} {{ $value->last_name }}</td>
+                                                <td>{{ $value->email }}</td>
+                                                <td>{{ $value->parent_name }}</td>
+                                                <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
+                                                <td style="min-width: 150px;">
+
+                                                    <a href="{{ url('admin/parent/assign_student_parent_delete/'.$value->id) }}"
+                                                        class="btn btn-danger btn-sm">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                <div style="padding: 10px; float:right;">
+        
+                                </div>
+                            </div>
+
+                            
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+
+
+        <!-- /.content -->
+    </div>
+@endsection
