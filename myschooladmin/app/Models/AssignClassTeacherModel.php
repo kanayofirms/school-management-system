5711341a3110d79c75b8e43aed69e2cf518c3047
kanayofirms <kanyeze@gmail.com>
2024-01-11 07:05:01 +0100

    Create model for assign classt teacher table in the database

---
+++ myschooladmin/app/Models/AssignClassTeacherModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AssignClassTeacherModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'assign_class_teacher';
+
+    static public function getAlreadyFirst($class_id, $teacher_id)
+    {
+        return self::where('class_id', '=', $class_id)->where('teacher_id', '=', $teacher_id)->first();
+    }
+}
