066339d41f8dd5a8cbf9df998a915fa55c073caf
kanayofirms <kanyeze@gmail.com>
2024-01-11 07:07:28 +0100

    Create insert function to insert data from assign class teacher to the database

--- myschooladmin/app/Http/Controllers/AssignClassTeacherController.php
+++ myschooladmin/app/Http/Controllers/AssignClassTeacherController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\AssignClassTeacherModel;
 use App\Models\ClassModel;
 use App\Models\User;
+use Auth;
 
 class AssignClassTeacherController extends Controller
 {
@@ -20,5 +22,35 @@ class AssignClassTeacherController extends Controller
         $data['getTeacher'] = User::getTeacherClass();
         $data['header_title'] = "Add Assign Class Teacher";
         return view('admin.assign_class_teacher.add', $data);
+    }
+
+    public function insert(Request $request)
+    {
+        if(!empty($request->teacher_id))
+        {
+            foreach ($request->teacher_id as $teacher_id) 
+            {
+                $getAlreadyFirst = AssignClassTeacherModel::getAlreadyFirst($request->class_id, $teacher_id);
+                if(!empty($getAlreadyFirst))
+                {
+                    $getAlreadyFirst->status = $request->status;
+                    $getAlreadyFirst->save();
+                }
+                else
+                {
+                    $save = new AssignClassTeacherModel;
+                    $save->class_id = $request->class_id;
+                    $save->teacher_id = $teacher_id;
+                    $save->status = $request->status;
+                    $save->created_by = Auth::user()->id;
+                    $save->save();
+                }
+            }
+            return redirect('admin/assign_class_teacher/list')->with('success', "Assign Class to Teacher Successfully");
+        }
+        else
+        {
+            return redirect()->back()->with('error', "Due to some error please try again");
+        }
     }
 }
