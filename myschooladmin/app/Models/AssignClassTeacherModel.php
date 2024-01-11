022cebcf5f07c9b1c5adc3fc28e551ebdb5a2b52
kanayofirms <kanyeze@gmail.com>
2024-01-11 08:31:53 +0100

    Create getRecord function to fetch teacher data by id

--- myschooladmin/app/Models/AssignClassTeacherModel.php
+++ myschooladmin/app/Models/AssignClassTeacherModel.php
@@ -11,6 +11,20 @@ class AssignClassTeacherModel extends Model
 
     protected $table = 'assign_class_teacher';
 
+    static public function getRecord()
+    {
+        $return = self::select('assign_class_teacher.*', 'class.name as class_name', 'teacher.name as teacher_name', 'users.name as created_by_name')
+                    ->join('users as teacher', 'teacher.id', '=', 'assign_class_teacher.teacher_id')
+                    ->join('class', 'class.id', '=', 'assign_class_teacher.class_id')
+                    ->join('users', 'users.id', '=', 'assign_class_teacher.created_by')
+                    ->where('assign_class_teacher.is_delete', '=', 0);
+
+        $return = $return->orderBy('assign_class_teacher.id', 'desc')
+                    ->paginate(20);
+
+        return $return;
+    }
+
     static public function getAlreadyFirst($class_id, $teacher_id)
     {
         return self::where('class_id', '=', $class_id)->where('teacher_id', '=', $teacher_id)->first();
