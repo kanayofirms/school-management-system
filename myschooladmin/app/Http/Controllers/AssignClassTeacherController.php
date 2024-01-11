44ba82976531105bee856b69f55b9ffe6d281252
kanayofirms <kanyeze@gmail.com>
2024-01-11 08:28:26 +0100

    Refactor insert function code for functionality

--- myschooladmin/app/Http/Controllers/AssignClassTeacherController.php
+++ myschooladmin/app/Http/Controllers/AssignClassTeacherController.php
@@ -12,6 +12,7 @@ class AssignClassTeacherController extends Controller
 {
     public function list(Request $request)
     {
+        $data['getRecord'] = AssignClassTeacherModel::getRecord();
         $data['header_title'] = "Assign Class Teacher";
         return view('admin.assign_class_teacher.list', $data); 
     }
