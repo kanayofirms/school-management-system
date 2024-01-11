06588303a33ddd3e5e5df23e84b4333842fa66c9
kanayofirms <kanyeze@gmail.com>
2024-01-11 12:38:05 +0100

    Create getSingle function and refactor getAssignTeacherID function for feasibility

--- myschooladmin/app/Models/AssignClassTeacherModel.php
+++ myschooladmin/app/Models/AssignClassTeacherModel.php
@@ -11,6 +11,11 @@ class AssignClassTeacherModel extends Model
 
     protected $table = 'assign_class_teacher';
 
+    static public function getSingle($id)
+    {
+        return self::find($id);
+    }
+
     static public function getRecord()
     {
         $return = self::select('assign_class_teacher.*', 'class.name as class_name', 'teacher.name as teacher_name', 'users.name as created_by_name')
@@ -28,5 +33,10 @@ class AssignClassTeacherModel extends Model
     static public function getAlreadyFirst($class_id, $teacher_id)
     {
         return self::where('class_id', '=', $class_id)->where('teacher_id', '=', $teacher_id)->first();
+    }
+
+    static public function getAssignTeacherID($class_id)
+    {
+        return self::where('class_id', '=', $class_id)->where('is_delete', '=', 0)->get();
     }
 }
