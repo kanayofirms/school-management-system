8405a4a424fec7d1662ef7f2e015a8411907a01e
kanayofirms <kanyeze@gmail.com>
2024-01-12 12:52:22 +0100

    Create dashboard menu for my class and subject in teacher dashboard

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -257,7 +257,17 @@
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
-                <span class="badge badge-info right">2</span>
+                
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="{{ url('teacher/my_class_subject') }}" class="nav-link @if(Request::segment(2) == 'my_class_subject') active @endif">
+              <i class="nav-icon far fa-user"></i>
+              <p>
+                My Class & Subject
+                
               </p>
             </a>
           </li>
