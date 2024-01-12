53f41c5c812a45bfe8299d8117507bb527db8c5b
kanayofirms <kanyeze@gmail.com>
2024-01-12 07:21:36 +0100

    Create POST route to update single class teacher list

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -126,6 +126,7 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('admin/assign_class_teacher/edit/{id}', [AssignClassTeacherController::class, 'edit']);
     Route::post('admin/assign_class_teacher/edit/{id}', [AssignClassTeacherController::class, 'update']);
     Route::get('admin/assign_class_teacher/edit_single/{id}', [AssignClassTeacherController::class, 'edit_single']);
+    Route::post('admin/assign_class_teacher/edit_single/{id}', [AssignClassTeacherController::class, 'update_single']);
 
 
 }); 
