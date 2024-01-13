9b0543c5771a5b7ba66929ef6d4b3e64f58d4210
kanayofirms <kanyeze@gmail.com>
2024-01-13 07:59:52 +0100

    Edit card header for readability

--- myschooladmin/resources/views/teacher/my_student.blade.php
+++ myschooladmin/resources/views/teacher/my_student.blade.php
@@ -31,7 +31,7 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Student List</h3>
+                                <h3 class="card-title">My Student List</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body p-0" style="overflow: auto;">
