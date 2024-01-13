5ab710505ddcfd8e2c4838df18ce0ae338b397ab
kanayofirms <kanyeze@gmail.com>
2024-01-13 23:34:48 +0100

    Get class record from classmodel in the database

--- myschooladmin/app/Http/Controllers/ClassTimetableController.php
+++ myschooladmin/app/Http/Controllers/ClassTimetableController.php
@@ -3,18 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ClassModel;
 
 class ClassTimetableController extends Controller
 {
     public function list()
     {
+        $data['getClass'] = ClassModel::getClass();
         $data['header_title'] = "Class Timetable List";
         return view('admin.class_timetable.list', $data); 
     }
 
-    public function add()
-    {
-        $data['header_title'] = "Add Class Timetable";
-        return view('admin.class_timetable.add', $data);
-    }
+    
 }
