eb9f6666d4457c0a50db1de5025884fb3f8a2370
kanayofirms <kanyeze@gmail.com>
2024-01-13 07:57:24 +0100

    Join groupBy to the getTeacherStudent query and refactor code for scalability

--- myschooladmin/app/Models/User.php
+++ myschooladmin/app/Models/User.php
@@ -261,9 +261,12 @@ class User extends Authenticatable
                         ->join('class', 'class.id', '=', 'users.class_id')
                         ->join('assign_class_teacher', 'assign_class_teacher.class_id', '=', 'class.id')
                         ->where('assign_class_teacher.teacher_id', '=', $teacher_id)
+                        ->where('assign_class_teacher.status', '=', 0)
+                        ->where('assign_class_teacher.is_delete', '=', 0)
                         ->where('users.user_type', '=', 3)
                         ->where('users.is_delete', '=', 0);
         $return = $return->orderBy('users.id', 'desc')
+                        ->groupBy('users.id')
                         ->paginate(20);
 
         return $return;
