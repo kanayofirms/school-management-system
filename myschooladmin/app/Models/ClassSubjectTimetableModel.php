e038dd52d6dca6e7db8478404ff1e0fa380fd70f
kanayofirms <kanyeze@gmail.com>
2024-01-14 17:32:09 +0100

    Create function to get record for class subject

--- myschooladmin/app/Models/ClassSubjectTimetableModel.php
+++ myschooladmin/app/Models/ClassSubjectTimetableModel.php
@@ -10,4 +10,11 @@ class ClassSubjectTimetableModel extends Model
     use HasFactory;
 
     protected $table = 'class_subject_timetable';
+
+    static public function getRecordClassSubject($class_id, $subject_id, $week_id)
+    {
+        return self::where('class_id', '=', $class_id)->where('subject_id', '=', $subject_id)->where('week_id', '=', $week_id)->first();
+    }
 }
+
+
