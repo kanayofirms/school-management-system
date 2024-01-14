14a709476c83f3575caf848e91e602b5e278e378
kanayofirms <kanyeze@gmail.com>
2024-01-14 15:47:31 +0100

    Create model for class subject timetable table in database

---
+++ myschooladmin/app/Models/ClassSubjectTimetableModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassSubjectTimetableModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'class_subject_timetable';
+}
