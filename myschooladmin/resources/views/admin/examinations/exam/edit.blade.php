bca72b14efcbbda93a31ad1f83795e69680b8290
kanayofirms <kanyeze@gmail.com>
2024-01-17 11:30:38 +0100

    Create edit view to edit exam on admin side

---
+++ myschooladmin/resources/views/admin/examinations/exam/edit.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+ 
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Edit Exam</h1>
+          </div>
+        
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">   
+              <form method="post" action="">
+                {{ csrf_field() }}
+                <div class="card-body">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" name="name" value="{{ $getRecord->name }}" placeholder="Name">
+                      </div>
+
+                      <div class="form-group">
+                        <label>Note</label>
+                        <textarea name="note" class="form-control" placeholder="Note">{{ $getRecord->note }}</textarea>
+                        </div>
+                    </div>
+                  
+                  
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+              </form>
+            </div>
+            
+
+          </div>
+          <!--/.col (left) -->
+          <!-- right column -->
+          
+          <!--/.col (right) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+
+@endsection
