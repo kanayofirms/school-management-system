925aea8c5c4a6d73d7edca1dcccbea63a9bba7e3
kanayofirms <kanyeze@gmail.com>
2024-01-17 23:51:46 +0100

    Create GET route for exam schedule

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -144,11 +144,7 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('admin/examinations/exam/edit/{id}', [ExaminationsController::class, 'exam_update']);
     Route::get('admin/examinations/exam/delete/{id}', [ExaminationsController::class, 'exam_delete']);
 
-
-
-
-
-
+    Route::get('admin/examinations/exam_schedule', [ExaminationsController::class, 'exam_schedule']);
 
 
 }); 
