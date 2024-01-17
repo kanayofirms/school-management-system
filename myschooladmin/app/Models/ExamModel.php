ea5e68e7dfd3d98c2aa4a4a6ff216c8be44239a8
kanayofirms <kanyeze@gmail.com>
2024-01-17 22:36:34 +0100

    Create conditional statements for filter in exam list

--- myschooladmin/app/Models/ExamModel.php
+++ myschooladmin/app/Models/ExamModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class ExamModel extends Model
 {
@@ -18,10 +19,25 @@ class ExamModel extends Model
 
     static public function getRecord()
     {
-        return self::select('exam.*', 'users.name as created_name')
-                ->join('users', 'users.id', '=', 'exam.created_by')
-                ->where('exam.is_delete', '=', 0)
+        $return = self::select('exam.*', 'users.name as created_name')
+                ->join('users', 'users.id', '=', 'exam.created_by');
+
+                if(!empty(Request::get('name')))
+                {
+                    $return = $return->where('exam.name', 'like', '%'.Request::get('name').'%');
+
+                }
+                    
+
+                if(!empty(Request::get('date')))
+                {
+                    $return = $return->whereDate('exam.created_at', '=', Request::get('date'));
+                }
+
+
+                $return = $return->where('exam.is_delete', '=', 0)
                 ->orderBy('exam.id', 'desc')
                 ->paginate(50);
+        return $return;
     }
 }
