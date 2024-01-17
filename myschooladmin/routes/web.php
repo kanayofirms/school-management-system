516bc5bed35d10b8e2db39fd5bce0012d0ae1403
kanayofirms <kanyeze@gmail.com>
2024-01-17 12:22:01 +0100

    Create GET route to delete exam on admin side

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -142,6 +142,8 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('admin/examinations/exam/add', [ExaminationsController::class, 'exam_insert']);
     Route::get('admin/examinations/exam/edit/{id}', [ExaminationsController::class, 'exam_edit']);
     Route::post('admin/examinations/exam/edit/{id}', [ExaminationsController::class, 'exam_update']);
+    Route::get('admin/examinations/exam/delete/{id}', [ExaminationsController::class, 'exam_delete']);
+
 
 
 
