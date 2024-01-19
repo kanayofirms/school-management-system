ee34669438265e1f75b84c437ec690851710af77
kanayofirms <kanyeze@gmail.com>
2024-01-19 16:14:26 +0100

    Create my exam timetable menu on student side

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -366,6 +366,15 @@
         </li>
 
         <li class="nav-item">
+          <a href="{{ url('student/my_exam_timetable') }}" class="nav-link @if(Request::segment(2) == 'my_exam_timetable') active @endif">
+            <i class="nav-icon far fa-user"></i>
+            <p>
+              My Exam Timetable
+            </p>
+          </a>
+        </li>
+
+        <li class="nav-item">
           <a href="{{ url('student/account') }}" class="nav-link @if(Request::segment(2) == 'account') active @endif">
             <i class="nav-icon far fa-user"></i>
             <p>
