7cbb3e1dd864df0702bd0654a1201b98f074e099
kanayofirms <kanyeze@gmail.com>
2024-01-19 14:51:46 +0100

    Create delete record function to delete double entry from query

--- myschooladmin/app/Models/ExamScheduleModel.php
+++ myschooladmin/app/Models/ExamScheduleModel.php
@@ -10,4 +10,14 @@ class ExamScheduleModel extends Model
     use HasFactory;
 
     protected $table = 'exam_schedule';
+
+    static public function getRecordSingle($exam_id, $class_id, $subject_id)
+    {
+        return ExamScheduleModel::where('exam_id', '=', $exam_id)->where('class_id', '=', $class_id)->where('subject_id', '=', $subject_id)->first();
+    }
+
+    static public function deleteRecord($exam_id, $class_id)
+    {
+        ExamScheduleModel::where('exam_id', '=', $exam_id)->where('class_id', '=', $class_id)->delete();
+    }
 }
