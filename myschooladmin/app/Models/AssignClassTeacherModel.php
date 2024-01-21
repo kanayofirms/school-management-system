edfcfea345e89c55d62149ad845f0d02e90ba602
kanayofirms <kanyeze@gmail.com>
2024-01-21 16:57:39 +0100

    Create getMyClassSubjectGroup function and pass teacher id as parameter

--- myschooladmin/app/Models/AssignClassTeacherModel.php
+++ myschooladmin/app/Models/AssignClassTeacherModel.php
@@ -64,6 +64,17 @@ class AssignClassTeacherModel extends Model
                     ->get();
     }
 
+    static public function getMyClassSubjectGroup($teacher_id)
+    {
+        return AssignClassTeacherModel::select('assign_class_teacher.*', 'class.name as class_name', 'class.id as class_id')
+                    ->join('class', 'class.id', '=', 'assign_class_teacher.class_id')
+                   ->where('assign_class_teacher.is_delete', '=', 0)
+                   ->where('assign_class_teacher.status', '=', 0)
+                   ->where('assign_class_teacher.teacher_id', '=', $teacher_id)
+                   ->groupBy('assign_class_teacher.class_id')
+                    ->get();
+    }
+
     static public function getAlreadyFirst($class_id, $teacher_id)
     {
         return self::where('class_id', '=', $class_id)->where('teacher_id', '=', $teacher_id)->first();
