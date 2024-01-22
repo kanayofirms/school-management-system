b3d39d50cccabbf33e663996d02024f8c96c767c
kanayofirms <kanyeze@gmail.com>
2024-01-22 09:48:49 +0100

    Create GET route for exam timetable on parent side

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -200,6 +200,9 @@ Route::group(['middleware' => 'parent'], function () {
 
     Route::get('parent/my_student/subject/{student_id}', [SubjectController::class, 'parentStudentSubject']);
 
+    Route::get('parent/my_student/exam_timetable/{student_id}', [ExaminationsController::class, 'myExamTimetableParent']);
+
+
     Route::get('parent/my_student/subject/class_timetable/{class_id}/{subject_id}/{student_id}', [ClassTimetableController::class, 'myTimetableParent']);
 
 
