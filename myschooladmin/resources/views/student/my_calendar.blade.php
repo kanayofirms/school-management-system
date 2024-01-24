5ca6fc66f554ae599643574ce25b70b14b5f95c9
kanayofirms <kanyeze@gmail.com>
2024-01-24 06:54:44 +0100

    Create view for my calendar on student side

---
+++ myschooladmin/resources/views/student/my_calendar.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>My Calendar</h1>
+                    </div>
+
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div id="calendar"></div>
+                    </div>     
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+    </div>
+@endsection
+
+@section('script')
+<script src="{{ url('public/dist/fullcalendar/index.global.js') }}"></script>
+<script type="text/javascript">
+    var calendarID = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarID, {
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+        },
+    });
+
+    calendar.render();
+</script>
+@endsection
