b2ce5ee8bb370140059223c82fc6652bf0b1b582
kanayofirms <kanyeze@gmail.com>
2024-01-26 15:20:59 +0100

    Create marks register menu on admin side

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -261,6 +261,17 @@
               </li>
             </ul>
           </li>
+
+          <li class="nav-item">
+            <a href="{{ url('admin/examinations/marks_register') }}" class="nav-link @if(Request::segment(3) == 'marks_register') active @endif">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Marks Register</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+
             </ul>
           </li>
 
