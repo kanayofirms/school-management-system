39942cd950eb7ec4289b8e64e128a2e750c82987
kanayofirms <kanyeze@gmail.com>
2024-01-26 09:01:30 +0100

    Create menu for my calendar on teacher side

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -336,6 +336,15 @@
             </a>
           </li>
 
+           <li class="nav-item">
+          <a href="{{ url('teacher/my_calendar') }}" class="nav-link @if(Request::segment(2) == 'my_calendar') active @endif">
+            <i class="nav-icon far fa-user"></i>
+            <p>
+              My Calendar
+            </p>
+          </a>
+        </li>
+
           <li class="nav-item">
             <a href="{{ url('teacher/change_password') }}" class="nav-link @if(Request::segment(2) == 'change_password') active @endif">
               <i class="nav-icon far fa-user"></i>
