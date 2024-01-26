383f6650be0ecc3f199ec3bd76421dd7ab167406
kanayofirms <kanyeze@gmail.com>
2024-01-26 15:14:04 +0100

    Create view for marks register

---
+++ myschooladmin/resources/views/admin/examinations/marks_register.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Marks Register</h1>
+                    </div>
+                </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- /.col -->
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Search Marks Register</h3>
+                            </div>
+                            <form method="get" action="">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-3">
+                                            <label>Exam</label>
+                                            <select class="form-control" name="exam_id" required>
+                                                <option value="">Select</option>
+                                                @foreach ($getExam as $exam)
+                                                    <option {{ (Request::get('exam_id') == $exam->id) ? 'selected' : '' }} value="{{ $exam->id }}">{{ $exam->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label>Class</label>
+                                            <select class="form-control" name="class_id" required>
+                                                <option value="">Select</option>
+                                                @foreach ($getClass as $class)
+                                                    <option {{ (Request::get('class_id') == $class->id) ? 'selected' : '' }} value="{{ $class->id }}">{{ $class->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-md-3">
+                                            <button class="btn btn-primary" type="submit"
+                                                style="margin-top: 30px;">Search</button>
+                                            <a href="{{ url('admin/examinations/marks_register') }}" class="btn btn-success"
+                                                style="margin-top: 30px;">Reset</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                        @include('_message')
+
+                            {{ csrf_field() }}
+                            <input type="hidden" name="exam_id" value="{{ Request::get('exam_id') }}">
+                            <input type="hidden" name="class_id" value="{{ Request::get('class_id') }}">
+                      
+                        
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Marks Register</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Subject Name</th>
+                                            <th>Exam Date</th>
+                                            <th>Start Time</th>
+                                            <th>End Time</th>
+                                            <th>Class Room</th>
+                                            <th>Full Marks</th>
+                                            <th>Passing Marks</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                       
+                                    </tbody>     
+                                </table>
+
+                            </div>
+                           
+                        </div>
+
+                      
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+
+
+        <!-- /.content -->
+    </div>
+@endsection
