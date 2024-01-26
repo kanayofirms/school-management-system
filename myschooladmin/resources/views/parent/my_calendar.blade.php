e8eaeac1d2e46d31306c2985b2270a2888180746
kanayofirms <kanyeze@gmail.com>
2024-01-26 07:23:33 +0100

    Refactor view for readability

--- myschooladmin/resources/views/parent/my_calendar.blade.php
+++ myschooladmin/resources/views/parent/my_calendar.blade.php
@@ -40,6 +40,29 @@
 <script type="text/javascript">
     var events = new Array();
 
+    
+    @foreach ($getMyTimetable as $value)
+        @foreach ($value['week'] as $week)
+            events.push({
+            title: '{{ $value['name'] }}', 
+            daysOfWeek: [ {{ $week['fullcalendar_day'] }} ],
+            startTime: '{{ $week['start_time'] }}',
+            endTime: '{{ $week['end_time'] }}', 
+             });
+        @endforeach       
+    @endforeach
+
+    @foreach ($getExamTimetable as $valueE)
+        @foreach ($valueE['exam'] as $exam)
+            events.push({
+                title: '{{ $valueE['name'] }} - {{ $exam['subject_name'] }} ({{ date('h:i A', strtotime($exam['start_time'])) 
+            }} to {{ date('h:i A', strtotime($exam['end_time'])) }})', 
+                start: '{{ $exam['exam_date'] }}',
+                end: '{{ $exam['exam_date'] }}',
+                color: '#f91d1d'
+            });
+        @endforeach       
+    @endforeach
    
     
     var calendarID = document.getElementById('calendar');
