daee5353dd1657c3eda934f75eddc6ea0e9451e8
kanayofirms <kanyeze@gmail.com>
2024-01-26 06:22:40 +0100

    Create button for calendar and style

--- myschooladmin/resources/views/parent/my_student.blade.php
+++ myschooladmin/resources/views/parent/my_student.blade.php
@@ -97,9 +97,12 @@
                                                 <td>{{ $value->genotype }}</td>
                                                 <td>{{ $value->disability }}</td>
                                                 <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
-                                                <td style="width:200rem;">
+                                                <td style="min-width:350px;">
                                                     <a class="btn btn-success btn-sm" href="{{ url('parent/my_student/subject/'.$value->id) }}">Subject</a>
                                                     <a class="btn btn-primary btn-sm" href="{{ url('parent/my_student/exam_timetable/'.$value->id) }}">Exam Timetable</a>
+                                                    
+                                                    <a class="btn btn-warning btn-sm" href="{{ url('parent/my_student/calendar/'.$value->id) }}">Calendar</a>
+
                                                 </td>
                                             </tr>
                                         @endforeach
