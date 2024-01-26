2ae624834790627532d57888c77c879ef9a1c13e
kanayofirms <kanyeze@gmail.com>
2024-01-26 11:24:08 +0100

    Create array to get exam timetable

--- myschooladmin/app/Http/Controllers/CalendarController.php
+++ myschooladmin/app/Http/Controllers/CalendarController.php
@@ -115,6 +115,7 @@ class CalendarController extends Controller
         $teacher_id = Auth::user()->id;
 
         $data['getClassTimetable'] = AssignClassTeacherModel::getCalendarTeacher($teacher_id);
+        $data['getExamTimetable'] = ExamScheduleModel::getExamTimetableTeacher($teacher_id);
 
         $data['header_title'] = "My Calendar";
         return view('teacher.my_calendar', $data);
