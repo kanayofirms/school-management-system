5cd2fc4dc6c22c749585aa5b8b74a5768dd781f2
kanayofirms <kanyeze@gmail.com>
2024-01-28 02:41:26 +0100

    Create getStudentClass function with class id

--- myschooladmin/app/Models/User.php
+++ myschooladmin/app/Models/User.php
@@ -255,6 +255,16 @@ class User extends Authenticatable
         return $return;
     }
 
+    static public function getStudentClass($class_id)
+    {
+        return self::select('users.id', 'users.name', 'users.middle_name', 'users.last_name')
+                        ->where('users.user_type', '=', 3)
+                        ->where('users.is_delete', '=', 0)
+                        ->where('users.class_id', '=', $class_id)
+                        ->orderBy('users.id', 'desc')
+                        ->get();
+    }
+
     static public function getTeacherStudent($teacher_id)
     {
         $return = self::select('users.*', 'class.name as class_name')
