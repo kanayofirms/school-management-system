85fd574868a8fae66f3006fe52797a3d1cae6364
kanayofirms <kanyeze@gmail.com>
2024-02-05 00:44:43 +0100

    Create getExamTeacher function with teacher id

--- myschooladmin/app/Models/ExamScheduleModel.php
+++ myschooladmin/app/Models/ExamScheduleModel.php
@@ -36,6 +36,17 @@ class ExamScheduleModel extends Model
             ->get();
     }
 
+    static public function getExamTeacher($teacher_id)
+    {
+        return ExamScheduleModel::select('exam_schedule.*', 'exam.name as exam_name')
+            ->join('exam', 'exam.id', '=', 'exam_schedule.exam_id')
+            ->join('assign_class_teacher', 'assign_class_teacher.class_id', '=', 'exam_schedule.class_id')
+            ->where('assign_class_teacher.teacher_id', '=', $teacher_id)
+            ->groupBy('exam_schedule.exam_id')
+            ->orderBy('exam_schedule.id', 'desc')
+            ->get();
+    }
+
     static public function getExamTimetable($exam_id, $class_id)
     {
         return ExamScheduleModel::select('exam_schedule.*', 'subject.name as subject_name', 'subject.type as 
