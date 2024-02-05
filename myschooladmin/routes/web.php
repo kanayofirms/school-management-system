e47e3a26c2425eeb2fbe053fc309bc5b3f26d564
kanayofirms <kanyeze@gmail.com>
2024-02-05 01:20:59 +0100

    Create POST route for single submit marks register on teacher side

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -177,6 +177,7 @@ Route::group(['middleware' => 'teacher'], function () {
 
     Route::get('teacher/marks_register', [ExaminationsController::class, 'marks_register_teacher']);
     Route::post('teacher/submit_marks_register', [ExaminationsController::class, 'submit_marks_register']);
+    Route::post('teacher/single_submit_marks_register', [ExaminationsController::class, 'single_submit_marks_register']);
 });
 
 Route::group(['middleware' => 'student'], function () {
