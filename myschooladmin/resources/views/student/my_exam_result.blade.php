c2635aab58ed14dbac85807ed40b2f9a0496d4da
kanayofirms <kanyeze@gmail.com>
2024-02-07 00:32:01 +0100

    Refactor view for feasibility

--- myschooladmin/resources/views/student/my_exam_result.blade.php
+++ myschooladmin/resources/views/student/my_exam_result.blade.php
@@ -17,31 +17,44 @@
     <section class="content">
       <div class="container-fluid">
         <div class="row"> 
+          @foreach ($getRecord as $value)      
           <div class="col-md-12">
-            <!-- /.card -->
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">My Subject</h3>
+                <h3 class="card-title">{{ $value['exam_name'] }}</h3>
               </div>
-              <!-- /.card-header -->
               <div class="card-body p-0">
                 <table class="table table-striped">
                   <thead>
                     <tr>
                       <th>Subject Name</th>
-                      <th>Subject Type</th>
+                      <th>Attendance</th>
+                      <th>CAT 1</th>
+                      <th>CAT 2</th>
+                      <th>Exam</th>
+                      <th>Passing Mark</th>
+                      <th>Full Mark</th>
+
                     </tr>
                   </thead>
                   <tbody>
-                       
+                    @foreach ($value['subject'] as $exam)
+                       <tr>
+                        <td>{{ $exam['subject_name'] }}</td>
+                        <td>{{ $exam['attendance'] }}</td>
+                        <td>{{ $exam['cat_one'] }}</td>
+                        <td>{{ $exam['cat_two'] }}</td>
+                        <td>{{ $exam['exam'] }}</td>
+                        <td>{{ $exam['passing_mark'] }}</td>
+                        <td>{{ $exam['full_mark'] }}</td>
+                       </tr>
+                    @endforeach
                   </tbody>
                 </table>               
-              <!-- /.card-body -->
             </div>
-            <!-- /.card -->
           </div>
-          <!-- /.col -->
         </div>
+        @endforeach
         <!-- /.row -->  
         <!-- /.row -->
       </div><!-- /.container-fluid -->
