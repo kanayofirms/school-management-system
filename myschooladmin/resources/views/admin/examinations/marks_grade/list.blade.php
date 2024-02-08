baafd9ddbd528b988135dbf0772a8ebac6fd7d21
kanayofirms <kanyeze@gmail.com>
2024-02-08 08:49:25 +0100

    Create views for marks grade

---
+++ myschooladmin/resources/views/admin/examinations/marks_grade/list.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Marks Grade</h1>
+                    </div>
+                    <div class="col-sm-6" style="text-align: right;">
+                        <a href="{{ url('admin/examinations/marks_grade/add') }}" class="btn btn-primary">Add New Marks Grade</a>
+
+                    </div>
+
+                </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+
+                    <!-- /.col -->
+                    <div class="col-md-12">
+
+                        @include('_message')
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Marks Grade List</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Grade Name</th>
+                                            <th>Percent From</th>
+                                            <th>Percent To</th>
+                                            <th>Created By</th>
+                                            <th>Created Date</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                       
+                                    </tbody>
+                                </table>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+
+
+        <!-- /.content -->
+    </div>
+@endsection
