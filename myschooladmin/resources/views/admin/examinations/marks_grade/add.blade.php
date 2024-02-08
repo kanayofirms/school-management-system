8b89465e75dfe1a8c01b1522b484e47679a162a6
kanayofirms <kanyeze@gmail.com>
2024-02-08 10:28:01 +0100

    Create views for marks grade app on admin side

---
+++ myschooladmin/resources/views/admin/examinations/marks_grade/add.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Add New Exam</h1>
+                    </div>
+
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- general form elements -->
+                        <div class="card card-primary">
+                            <form method="post" action="">
+                                {{ csrf_field() }}
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label>Grade Name</label>
+                                        <input type="number" class="form-control" value="{{ old('name') }}"
+                                            name="name" required placeholder="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Percent From</label>
+                                        <input type="number" class="form-control" value="{{ old('percent_from') }}"
+                                            name="percent_from" required placeholder="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Percent To</label>
+                                        <input type="text" class="form-control" value="{{ old('percent_to') }}"
+                                            name="percent_to" required placeholder="Exam Name">
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+
+                    </div>
+                    <!--/.col (left) -->
+                    <!-- right column -->
+
+                    <!--/.col (right) -->
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
