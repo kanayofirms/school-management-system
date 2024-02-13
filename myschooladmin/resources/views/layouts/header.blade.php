089ae405f5f14c695658082502839e469f938ce4
kanayofirms <kanyeze@gmail.com>
2024-02-13 06:47:39 +0100

    Create attendance menu with sub-menu

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -304,10 +304,26 @@
 
                     </ul>
                   </li>
-          
 
-
-
+                  <li class="nav-item @if (Request::segment(2) == 'attendance') menu-is-opening menu-open @endif">
+                    <a href="#" class="nav-link @if (Request::segment(2) == 'attendance') active @endif">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                            Attendance
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ url('admin/attendance/student') }}"
+                                class="nav-link @if (Request::segment(3) == 'student') active @endif">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Student Attendance</p>
+                            </a>
+                        </li>
+               </ul>
+             </li>
+     
 
              <li class="nav-item">
                  <a href="{{ url('admin/account') }}"
