25a44bf8cb666059c3a1aa6a3b5c8f428d5ae241
kanayofirms <kanyeze@gmail.com>
2024-02-13 08:11:30 +0100

    Refactor student attendace function to get class by id and attendance date

--- myschooladmin/app/Http/Controllers/AttendanceController.php
+++ myschooladmin/app/Http/Controllers/AttendanceController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ClassModel;
+use App\Models\User;
+
+
 
 
 class AttendanceController extends Controller
 {
-    public function attendance_student()
+    public function attendance_student(Request $request)
     {
         $data['getClass'] = ClassModel::getClass();
+        if(!empty($request->get('class_id')) && !empty($request->get('attendance_date')))
+        {
+            $data['getStudent'] = User::getStudentClass($request->get('class_id'));
+        }
         $data['header_title'] = "Student Attendance";
         return view('admin.attendance.student', $data);
     }
