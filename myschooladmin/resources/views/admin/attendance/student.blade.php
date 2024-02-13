0d7f678d7f84bc8f1c71c2eb2bb7c418db621935
kanayofirms <kanyeze@gmail.com>
2024-02-13 06:52:32 +0100

    Create views for student attendance on admin side

---
+++ myschooladmin/resources/views/admin/attendance/student.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Student Attendance</h1>
+                    </div>
+                </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- /.col -->
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Search Student Attendance</h3>
+                            </div>
+                            <form method="get" action="">
+                                <div class="card-body">
+                                    <div class="row">
+
+                                        <div class="form-group col-md-3">
+                                            <label>Class</label>
+                                            <select class="form-control" name="class_id" required>
+                                                <option value="">Select</option>
+                                                @foreach ($getClass as $class)
+                                                    <option {{ (Request::get('class_id') == $class->id) ? 'selected' : '' }} value="{{ $class->id }}">{{ $class->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label>Date</label>
+                                            <select class="form-control" name="class_id" required>
+                                                <option value="">Select</option>
+                                                @foreach ($getClass as $class)
+                                                    <option {{ (Request::get('class_id') == $class->id) ? 'selected' : '' }} value="{{ $class->id }}">{{ $class->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <button class="btn btn-primary" type="submit"
+                                                style="margin-top: 30px;">Search</button>
+                                            <a href="{{ url('admin/attendance/student') }}" class="btn btn-success"
+                                                style="margin-top: 30px;">Reset</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@section('script')
+
+<script type="text/javascript">
+  
+</script>
+
+@endsection
