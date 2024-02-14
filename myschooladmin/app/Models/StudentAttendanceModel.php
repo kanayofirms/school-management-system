cb03af1b6a0e9f3e611fe800b671567fb78e2a43
kanayofirms <kanyeze@gmail.com>
2024-02-14 18:49:17 +0100

    Create query for single entry to the db

--- myschooladmin/app/Models/StudentAttendanceModel.php
+++ myschooladmin/app/Models/StudentAttendanceModel.php
@@ -10,4 +10,11 @@ class StudentAttendanceModel extends Model
     use HasFactory;
 
     protected $table = 'student_attendance';
+
+    static public function CheckAlreadyAttendance($student_id, $class_id, $attendance_date)
+    {
+        return StudentAttendanceModel::where('student_id', '=', $student_id)->where('class_id', '=', $class_id)->
+            where('attendance_date', '=', $attendance_date)->first();
+
+    }
 }
