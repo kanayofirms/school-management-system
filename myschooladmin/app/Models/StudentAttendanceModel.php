e25f242496afd002084d542e17afc21f1cef4419
kanayofirms <kanyeze@gmail.com>
2024-02-15 15:06:58 +0100

    Create query to get created by

--- myschooladmin/app/Models/StudentAttendanceModel.php
+++ myschooladmin/app/Models/StudentAttendanceModel.php
@@ -17,4 +17,16 @@ class StudentAttendanceModel extends Model
             where('attendance_date', '=', $attendance_date)->first();
 
     }
+
+    static public function getRecord()
+    {
+        return StudentAttendanceModel::select('student_attendance.*', 'class.name as class_name', 'student.name as student_name', 
+            'student.middle_name as student_middle_name', 'student.last_name as student_last_name',
+            'createdby.name as created_name')
+            ->join('class', 'class.id', '=', 'student_attendance.class_id')
+            ->join('users as student', 'student.id', '=', 'student_attendance.student_id')
+            ->join('users as createdby', 'createdby.id', '=', 'student_attendance.created_by')
+            ->orderBy('student_attendance.id', 'desc')
+            ->paginate(50);
+    }
 }
