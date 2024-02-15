30179dd53624d6c7dbd8d192996552ee204413d1
kanayofirms <kanyeze@gmail.com>
2024-02-15 16:35:28 +0100

    Create pagination and update app

--- myschooladmin/resources/views/admin/attendance/report.blade.php
+++ myschooladmin/resources/views/admin/attendance/report.blade.php
@@ -112,6 +112,10 @@
                                             @endforelse
                                         </tbody>
                                     </table>
+
+                                    <div style="padding: 10px; float: right;">
+                                        {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
+                                    </div>
                                 </div>
                             </div>
                     
