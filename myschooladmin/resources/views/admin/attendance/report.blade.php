87031bd73e427571f616d2c05fe8d710c47b4e11
kanayofirms <kanyeze@gmail.com>
2024-02-16 08:25:30 +0100

    Create filters for student id and student name

--- myschooladmin/resources/views/admin/attendance/report.blade.php
+++ myschooladmin/resources/views/admin/attendance/report.blade.php
@@ -28,7 +28,19 @@
                                 <div class="card-body">
                                     <div class="row">
 
-                                        <div class="form-group col-md-3">
+                                        <div class="form-group col-md-2">
+                                            <label>Student ID</label>
+                                            <input type="text" class="form-control" placeholder="Student ID" value="{{ Request::get('student_id') }}" 
+                                            name="student_id">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Student Name</label>
+                                            <input type="text" class="form-control" placeholder="Student Name" value="{{ Request::get('student_name') }}" 
+                                            name="student_name">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
                                             <label>Class</label>
                                             <select class="form-control" name="class_id">
                                                 <option value="">Select</option>
@@ -39,13 +51,13 @@
                                             </select>
                                         </div>
 
-                                        <div class="form-group col-md-3">
+                                        <div class="form-group col-md-2">
                                             <label>Attendance Date</label>
                                             <input type="date" class="form-control" value="{{ Request::get('attendance_date') }}" 
                                             name="attendance_date">
                                         </div>
 
-                                        <div class="form-group col-md-3">
+                                        <div class="form-group col-md-2">
                                             <label>Attendance Type</label>
                                             <select class="form-control" name="attendance_type">
                                                 <option value="">Select</option>
@@ -56,7 +68,7 @@
                                             </select>
                                         </div>
 
-                                        <div class="form-group col-md-3">
+                                        <div class="form-group col-md-2">
                                             <button class="btn btn-primary" type="submit"
                                                 style="margin-top: 30px;">Search</button>
                                             <a href="{{ url('admin/attendance/report') }}" class="btn btn-success"
