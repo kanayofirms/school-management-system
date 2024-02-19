8e35b77c2f98040a4fc44735c634c245565691d4
kanayofirms <kanyeze@gmail.com>
2024-02-19 02:43:22 +0100

    Create menu for attendance report on teacher side

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -434,13 +434,13 @@
                         </a>
                     </li>
 
-                    {{-- <li class="nav-item">
+                    <li class="nav-item">
                         <a href="{{ url('teacher/attendance/report') }}"
                             class="nav-link @if (Request::segment(3) == 'report') active @endif">
                             <i class="far fa-circle nav-icon"></i>
                             <p>Attendance Report</p>
                         </a>
-                    </li> --}}
+                    </li>
            </ul>
          </li>
  
