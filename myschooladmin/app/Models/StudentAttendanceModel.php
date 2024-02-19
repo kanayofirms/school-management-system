d517d6c2525b0e0fe276fcfb7baf6bf760fca7f2
kanayofirms <kanyeze@gmail.com>
2024-02-19 03:44:40 +0100

    Create getRecordTeacher function with class ids

--- myschooladmin/app/Models/StudentAttendanceModel.php
+++ myschooladmin/app/Models/StudentAttendanceModel.php
@@ -64,4 +64,58 @@ class StudentAttendanceModel extends Model
             ->paginate(50);
         return $return;
     }
+
+    static public function getRecordTeacher($class_ids)
+    {
+        if(!empty($class_ids))
+        {
+            $return = StudentAttendanceModel::select('student_attendance.*', 'class.name as class_name', 
+                'student.name as student_name', 'student.middle_name as student_middle_name', 'student.last_name as 
+                student_last_name', 'createdby.name as created_name')
+                    ->join('class', 'class.id', '=', 'student_attendance.class_id')
+                    ->join('users as student', 'student.id', '=', 'student_attendance.student_id')
+                    ->join('users as createdby', 'createdby.id', '=', 'student_attendance.created_by')
+                    ->whereIn('student_attendance.class_id', $class_ids);
+
+                if(!empty(Request::get('student_id')))
+                {
+                    $return = $return->where('student_attendance.student_id', '=', Request::get('student_id'));
+                }
+
+                if(!empty(Request::get('student_name')))
+                {
+                    $return = $return->where('student.name', 'like', '%'.Request::get('student_name').'%');
+                }
+
+                if(!empty(Request::get('student_last_name')))
+                {
+                    $return = $return->where('student.last_name', 'like', '%'.Request::get('student_last_name').'%');
+                }
+
+
+                if(!empty(Request::get('class_id')))
+                {
+                    $return = $return->where('student_attendance.class_id', '=', Request::get('class_id'));
+                }
+
+                if(!empty(Request::get('attendance_date')))
+                {
+                    $return = $return->where('student_attendance.attendance_date', '=', Request::get('attendance_date'));
+                }
+
+                if(!empty(Request::get('attendance_type')))
+                {
+                    $return = $return->where('student_attendance.attendance_type', '=', Request::get('attendance_type'));
+                }
+
+            $return = $return->orderBy('student_attendance.id', 'desc')
+                        ->paginate(50);
+            return $return;
+        }
+        else
+        {
+            return "";
+        }
+    }
+            
 }
