a721cd085ff6d3cef108ffd662aac27983db9dd8
kanayofirms <kanyeze@gmail.com>
2024-02-22 14:07:58 +0100

    Create my attendance parent function and pass student id as parameter

--- myschooladmin/app/Http/Controllers/AttendanceController.php
+++ myschooladmin/app/Http/Controllers/AttendanceController.php
@@ -99,4 +99,15 @@ class AttendanceController extends Controller
         $data['header_title'] = "My Attendance";
         return view('student.my_attendance', $data);
     }
+
+    // parent side code
+
+    public function myAttendanceParent($student_id)
+    {
+        $data['getStudent'] = User::getSingle($student_id);
+        $data['getClass'] = StudentAttendanceModel::getClassStudent($student_id);
+        $data['getRecord'] = StudentAttendanceModel::getRecordStudent($student_id);
+        $data['header_title'] = "Student Attendance";
+        return view('parent.my_attendance', $data);
+    }
 }
