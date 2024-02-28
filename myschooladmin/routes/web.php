a2f2772d9f3e1bb987e73886ad201547c2c58430
kanayofirms <kanyeze@gmail.com>
2024-02-28 15:04:16 +0100

    Create GET route to delete notice board on admin side

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -171,6 +171,8 @@ Route::group(['middleware' => 'admin'], function () {
 
     Route::get('admin/communicate/notice_board/edit/{id}', [CommunicateController::class, 'notice_board_edit']);
     Route::post('admin/communicate/notice_board/edit/{id}', [CommunicateController::class, 'notice_board_update']);
+    Route::get('admin/communicate/notice_board/delete/{id}', [CommunicateController::class, 'notice_board_delete']);
+
 });
 
 Route::group(['middleware' => 'teacher'], function () {
