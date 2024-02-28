07029b72964521a6b441729224e08c447671691f
kanayofirms <kanyeze@gmail.com>
2024-02-28 17:02:39 +0100

    Create filters for notice board

--- myschooladmin/resources/views/admin/communicate/noticeboard/list.blade.php
+++ myschooladmin/resources/views/admin/communicate/noticeboard/list.blade.php
@@ -22,6 +22,66 @@
 
                     <div class="col-md-12">
 
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Search Notice Board</h3>
+                            </div>
+                            <form method="get" action="">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-2">
+                                            <label>Title</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('title') }}"
+                                                name="title" placeholder="Title">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Notice Date To</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('notice_date_to') }}"
+                                                name="notice_date_to">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Notice Date From</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('notice_date_from') }}"
+                                                name="notice_date_from">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Publish Date To</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('publish_date_to') }}"
+                                                name="publish_date_to">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Publish Date From</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('publish_date_from') }}"
+                                                name="publish_date_from">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Message To</label>
+                                            <select class="form-control">
+                                                <option value="">Select</option>
+                                                <option value="2">Teacher</option>
+                                                <option value="3">Student</option>
+                                                <option value="4">Parent</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <button class="btn btn-primary" type="submit"
+                                                style="margin-top: 10px;">Search</button>
+                                            <a href="{{ url('admin/communicate/notice_board') }}" class="btn btn-success"
+                                                style="margin-top: 10px;">Reset</a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+
                         @include('_message')
 
                         <div class="card">
