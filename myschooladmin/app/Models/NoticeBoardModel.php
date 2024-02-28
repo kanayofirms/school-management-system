de00a84131f61b412897eac1bd8fe42b36fa1237
kanayofirms <kanyeze@gmail.com>
2024-02-28 17:44:26 +0100

    Create condition for notice from and notice to filters

--- myschooladmin/app/Models/NoticeBoardModel.php
+++ myschooladmin/app/Models/NoticeBoardModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class NoticeBoardModel extends Model
 {
@@ -18,10 +19,28 @@ class NoticeBoardModel extends Model
 
     static public function getRecord()
     {
-        return self::select('notice_board.*', 'users.name as created_by_name')
-                ->join('users', 'users.id', '=', 'notice_board.created_by')
-                ->orderBy('notice_board.id', 'desc')
+        $return = self::select('notice_board.*', 'users.name as created_by_name')
+                ->join('users', 'users.id', '=', 'notice_board.created_by');
+
+                if(!empty(Request::get('title')))
+                {
+                    $return = $return->where('notice_board.title', 'like', '%'.trim(Request::get('title')).'%');
+                }
+
+                if(!empty(Request::get('notice_date_from')))
+                {
+                    $return = $return->where('notice_board.notice_date', '>=', Request::get('notice_date_from'));
+                }
+
+                if(!empty(Request::get('notice_date_to')))
+                {
+                    $return = $return->where('notice_board.notice_date', '<=', Request::get('notice_date_to'));
+                }
+
+        $return = $return->orderBy('notice_board.id', 'desc')
                 ->paginate(20);
+
+        return $return;
     }
 
     public function getMessage()
