95fed3a8c3ec433f7741383b2418f911414a4347
kanayofirms <kanyeze@gmail.com>
2024-03-01 13:53:59 +0100

    Create filters and autoselection for values

--- myschooladmin/resources/views/student/my_notice_board.blade.php
+++ myschooladmin/resources/views/student/my_notice_board.blade.php
@@ -17,7 +17,47 @@
       <section class="content">
         <div class="container-fluid">
           <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Search Notice Board</h3>
+                    </div>
+                    <form method="get" action="">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="form-group col-md-3">
+                                    <label>Title</label>
+                                    <input type="text" class="form-control" value="{{ Request::get('title') }}"
+                                        name="title" placeholder="Title">
+                                </div>
+    
+                                <div class="form-group col-md-3">
+                                    <label>Notice Date From</label>
+                                    <input type="date" class="form-control" value="{{ Request::get('notice_date_from') }}"
+                                        name="notice_date_from">
+                                </div>
+                                
+                                <div class="form-group col-md-3">
+                                    <label>Notice Date To</label>
+                                    <input type="date" class="form-control" value="{{ Request::get('notice_date_to') }}"
+                                        name="notice_date_to">
+                                </div>
+    
+                                <div class="form-group col-md-3">
+                                    <button class="btn btn-primary" type="submit"
+                                        style="margin-top: 30px;">Search</button>
+                                    <a href="{{ url('student/my_notice_board') }}" class="btn btn-success"
+                                        style="margin-top: 30px;">Reset</a>
+                                </div>
+    
+                            </div>
+                        </div>
+    
+                    </form>
+                </div>
+            </div>
             
+                    
 
         @foreach ($getRecord as $value)
         <div class="col-md-12">
@@ -25,7 +65,7 @@
               <div class="card-body p-0">
                 <div class="mailbox-read-info">
                   <h5>{{ $value->title }}</h5>
-                  <h6><span class="mailbox-read-time float-right" style="font-weight: bold; color: #000; font-size: 16px;">{{ date('d-m-Y', strtotime($value->notice_date)) }}</span></h6>
+                  <h6 style="margin-top: 10px;">{{ date('d-m-Y', strtotime($value->notice_date)) }}</h6>
                 </div>
                 <div class="mailbox-read-message">
                   {!! $value->message !!}
