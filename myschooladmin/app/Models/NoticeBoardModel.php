fb4ee606cf8555e52779f7c4774d572de9312357
kanayofirms <kanyeze@gmail.com>
2024-03-01 11:22:24 +0100

    Create join and where query for the three records on student side

--- myschooladmin/app/Models/NoticeBoardModel.php
+++ myschooladmin/app/Models/NoticeBoardModel.php
@@ -65,7 +65,10 @@ class NoticeBoardModel extends Model
     {
         $return = self::select('notice_board.*', 'users.name as created_by_name')
                 ->join('users', 'users.id', '=', 'notice_board.created_by');
-
+        $return = $return->join('notice_board_message', 'notice_board_message.notice_board_id'
+                , '=', 'notice_board.id');
+        $return = $return->where('notice_board_message.message_to', '=', $message_to);
+        $return = $return->where('notice_board.publish_on', '<=', date('Y-m-d'));
         $return = $return->orderBy('notice_board.id', 'desc')
                 ->paginate(20);
 
