a2ea63b1a65c42f3d702d756b2ef3d429bcf6244
kanayofirms <kanyeze@gmail.com>
2024-03-02 05:28:58 +0100

    Create GET route for my notice board on parent side

--- myschooladmin/routes/web.php
+++ myschooladmin/routes/web.php
@@ -253,4 +253,6 @@ Route::group(['middleware' => 'parent'], function () {
     Route::get('parent/my_student/calendar/{student_id}', [CalendarController::class, 'myCalendarParent']);
 
     Route::get('parent/my_student/attendance/{student_id}', [AttendanceController::class, 'myAttendanceParent']);
+
+    Route::get('parent/my_notice_board', [CommunicateController::class, 'my_notice_board_parent']);
 });
