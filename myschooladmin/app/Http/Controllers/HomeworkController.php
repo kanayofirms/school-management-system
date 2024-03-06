f58f2a7566ba42e72ca1c6c7a4dd57380f6ed544
kanayofirms <kanyeze@gmail.com>
2024-03-06 16:15:36 +0100

    Create ajax function to insert data to the database

--- myschooladmin/app/Http/Controllers/HomeworkController.php
+++ myschooladmin/app/Http/Controllers/HomeworkController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ClassModel;
+use App\Models\ClassSubjectModel;
+
 
 class HomeworkController extends Controller
 {
@@ -18,5 +20,20 @@ class HomeworkController extends Controller
         $data['getClass'] = ClassModel::getClass();
         $data['header_title'] = "Add New Homework";
         return view('admin.homework.add', $data);
+    }
+
+    public function ajax_get_subject(Request $request)
+    {
+        $class_id = $request->class_id;
+        $getSubject = ClassSubjectModel::mySubject($class_id);
+        $html = '';
+        $html .= '<option value="">Select Subject</option>';
+        foreach ($getSubject as $value)
+        {
+            $html .= '<option value="'.$value->subject_id.'">'.$value->subject_name.'</option>';
+        }
+
+        $json['success'] = $html;
+        echo json_encode($json);
     }
 }
