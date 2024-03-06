adf6dc169f31905ead3ef278db303b3097276c2f
kanayofirms <kanyeze@gmail.com>
2024-03-06 17:11:31 +0100

    Create homework insert function and refactor for functionality

--- myschooladmin/app/Http/Controllers/HomeworkController.php
+++ myschooladmin/app/Http/Controllers/HomeworkController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ClassModel;
 use App\Models\ClassSubjectModel;
+use App\Models\HomeworkModel;
+use Illuminate\Support\Str;
+use Auth;
+
 
 
 class HomeworkController extends Controller
@@ -20,6 +24,33 @@ class HomeworkController extends Controller
         $data['getClass'] = ClassModel::getClass();
         $data['header_title'] = "Add New Homework";
         return view('admin.homework.add', $data);
+    }
+
+    public function homework_insert(Request $request)
+    {
+        $homework = new HomeworkModel;
+        $homework->class_id = trim($request->class_id);
+        $homework->subject_id = trim($request->subject_id);
+        $homework->homework_date = trim($request->homework_date);
+        $homework->submission_date = trim($request->submission_date);
+        $homework->description = trim($request->description);
+        $homework->created_by = Auth::user()->id;
+
+        if(!empty($request->file('document_file')))
+        {
+            $ext = $request->file('document_file')->getClientOriginalExtension();
+            $file = $request->file('document_file');
+            $randomStr = date('Ymdhis').Str::random(20);
+            $filename = strtolower($randomStr).'.'.$ext;
+            $file->move('upload/homework/', $filename);
+
+            $homework->document_file = $filename;
+        }
+
+        $homework->save();
+
+        return redirect('admin/homework/homework')->with('success', "Homework successfully created");
+
     }
 
     public function ajax_get_subject(Request $request)
