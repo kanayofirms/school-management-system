4d45b3d8a0f16342aa6bb490551566697d91e872
kanayofirms <kanyeze@gmail.com>
2024-03-07 03:12:25 +0100

    Create get record function and join query

--- myschooladmin/app/Models/HomeworkModel.php
+++ myschooladmin/app/Models/HomeworkModel.php
@@ -11,4 +11,17 @@ class HomeworkModel extends Model
 
     protected $table = 'homework';
 
+    static public function getRecord()
+    {
+        $return = self::select('homework.*', 'class.name as class_name', 'subject.name 
+        as subject_name', 'users.name as created_by_name')
+            ->join('users', 'users.id', '=', 'homework.created_by')
+            ->join('class', 'class.id', '=', 'homework.class_id')
+            ->join('subject', 'subject.id', '=', 'homework.subject_id')
+            ->where('homework.is_delete', '=', 0)
+            ->orderBy('homework.id', 'desc')
+            ->paginate(20);
+
+        return $return;
+    }
 }
