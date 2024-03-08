f3ca598095fe97d06f864d66adc5c1559d01334b
kanayofirms <kanyeze@gmail.com>
2024-03-08 13:27:22 +0100

    Create if condition for filters

--- myschooladmin/app/Models/HomeworkModel.php
+++ myschooladmin/app/Models/HomeworkModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class HomeworkModel extends Model
 {
@@ -23,8 +24,50 @@ class HomeworkModel extends Model
             ->join('users', 'users.id', '=', 'homework.created_by')
             ->join('class', 'class.id', '=', 'homework.class_id')
             ->join('subject', 'subject.id', '=', 'homework.subject_id')
-            ->where('homework.is_delete', '=', 0)
-            ->orderBy('homework.id', 'desc')
+            ->where('homework.is_delete', '=', 0);
+
+            if(!empty(Request::get('class_name')))
+            {
+                $return = $return->where('class.name', 'like', '%'.Request::get('class_name').'%');
+            }
+
+            if(!empty(Request::get('subject_name')))
+            {
+                $return = $return->where('subject.name', 'like', '%'.Request::get('subject_name').'%');
+            }
+
+            if(!empty(Request::get('from_homework_date')))
+            {
+                $return = $return->where('homework.homework_date', '>=', Request::get('from_homework_date'));
+            }
+
+            if(!empty(Request::get('to_homework_date')))
+            {
+                $return = $return->where('homework.homework_date', '<=', Request::get('to_homework_date'));
+            }
+
+            if(!empty(Request::get('from_submission_date')))
+            {
+                $return = $return->where('homework.submission_date', '>=', Request::get('from_submission_date'));
+            }
+
+            if(!empty(Request::get('to_submission_date')))
+            {
+                $return = $return->where('homework.submission_date', '<=', Request::get('to_submission_date'));
+            }
+
+            if(!empty(Request::get('from_created_date')))
+            {
+                $return = $return->whereDate('homework.created_at', '>=', Request::get('from_created_date'));
+            }
+
+            if(!empty(Request::get('to_created_date')))
+            {
+                $return = $return->whereDate('homework.created_at', '<=', Request::get('to_created_date'));
+            }
+
+
+            $return = $return->orderBy('homework.id', 'desc')
             ->paginate(20);
 
         return $return;
