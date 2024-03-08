e462dd3a6353f13ee024d9fbb8f322cd16fb0056
kanayofirms <kanyeze@gmail.com>
2024-03-08 13:28:20 +0100

    Create filters in view

--- myschooladmin/resources/views/admin/homework/list.blade.php
+++ myschooladmin/resources/views/admin/homework/list.blade.php
@@ -21,8 +21,74 @@
                 <div class="row">
 
                     <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Search Homework</h3>
+                            </div>
+                            <form method="get" action="">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-2">
+                                            <label>Class</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('class_name') }}"
+                                                name="class_name" placeholder="Class Name">
+                                        </div>
 
-                       
+                                        <div class="form-group col-md-2">
+                                            <label>Subject</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('subject_name') }}"
+                                                name="subject_name" placeholder="Subject Name">
+                                        </div>
+
+
+                                        <div class="form-group col-md-2">
+                                            <label>From Homework Date</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('from_homework_date') }}"
+                                                name="from_homework_date">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>To Homework Date</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('to_homework_date') }}"
+                                                name="to_homework_date">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>From Submission Date</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('from_submission_date') }}"
+                                                name="from_submission_date">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>To Submission Date</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('to_submission_date') }}"
+                                                name="to_submission_date">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>From Created Date</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('from_created_date') }}"
+                                                name="from_created_date">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>To Created Date</label>
+                                            <input type="date" class="form-control" value="{{ Request::get('to_created_date') }}"
+                                                name="to_created_date">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <button class="btn btn-primary" type="submit"
+                                                style="margin-top: 30px;">Search</button>
+                                            <a href="{{ url('admin/homework/homework') }}" class="btn btn-success"
+                                                style="margin-top: 30px;">Reset</a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
 
                         @include('_message')
 
