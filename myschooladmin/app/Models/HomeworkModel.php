2f5d108d031b8d9bfcb494731f881936d7aeff17
kanayofirms <kanyeze@gmail.com>
2024-03-09 14:09:34 +0100

    Create function to get record teacher by class ids

--- myschooladmin/app/Models/HomeworkModel.php
+++ myschooladmin/app/Models/HomeworkModel.php
@@ -73,6 +73,62 @@ class HomeworkModel extends Model
         return $return;
     }
 
+    static public function getRecordTeacher()
+    {
+        $return = self::select('homework.*', 'class.name as class_name', 'subject.name 
+        as subject_name', 'users.name as created_by_name')
+            ->join('users', 'users.id', '=', 'homework.created_by')
+            ->join('class', 'class.id', '=', 'homework.class_id')
+            ->join('subject', 'subject.id', '=', 'homework.subject_id')
+            ->where('homework.is_delete', '=', 0);
+
+            if(!empty(Request::get('class_name')))
+            {
+                $return = $return->where('class.name', 'like', '%'.Request::get('class_name').'%');
+            }
+
+            if(!empty(Request::get('subject_name')))
+            {
+                $return = $return->where('subject.name', 'like', '%'.Request::get('subject_name').'%');
+            }
+
+            if(!empty(Request::get('from_homework_date')))
+            {
+                $return = $return->where('homework.homework_date', '>=', Request::get('from_homework_date'));
+            }
+
+            if(!empty(Request::get('to_homework_date')))
+            {
+                $return = $return->where('homework.homework_date', '<=', Request::get('to_homework_date'));
+            }
+
+            if(!empty(Request::get('from_submission_date')))
+            {
+                $return = $return->where('homework.submission_date', '>=', Request::get('from_submission_date'));
+            }
+
+            if(!empty(Request::get('to_submission_date')))
+            {
+                $return = $return->where('homework.submission_date', '<=', Request::get('to_submission_date'));
+            }
+
+            if(!empty(Request::get('from_created_date')))
+            {
+                $return = $return->whereDate('homework.created_at', '>=', Request::get('from_created_date'));
+            }
+
+            if(!empty(Request::get('to_created_date')))
+            {
+                $return = $return->whereDate('homework.created_at', '<=', Request::get('to_created_date'));
+            }
+
+
+            $return = $return->orderBy('homework.id', 'desc')
+            ->paginate(20);
+
+        return $return;
+    }
+
     public function getDocument()
     {
         if(!empty($this->document_file) && file_exists('upload/homework/'.$this->document_file))
