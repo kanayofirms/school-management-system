7c12454cbef349dd8458829f63c3268a34c1f725
kanayofirms <kanyeze@gmail.com>
2024-03-09 07:27:08 +0100

    Create homework menu on teacher side

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -358,8 +358,8 @@
                             <p>Send Email</p>
                         </a>
                     </li>
-           </ul>
-         </li>
+                </ul>
+            </li>
 
          <li class="nav-item @if (Request::segment(2) == 'homework') menu-is-opening menu-open @endif">
             <a href="#" class="nav-link @if (Request::segment(2) == 'homework') active @endif">
@@ -378,8 +378,8 @@
                         <p>Homework</p>
                     </a>
                 </li>
-       </ul>
-     </li>
+            </ul>
+        </li>
      
 
              <li class="nav-item">
@@ -489,8 +489,28 @@
                             <p>Attendance Report</p>
                         </a>
                     </li>
-           </ul>
-         </li>
+                </ul>
+                </li>
+
+         <li class="nav-item @if (Request::segment(2) == 'homework') menu-is-opening menu-open @endif">
+            <a href="#" class="nav-link @if (Request::segment(2) == 'homework') active @endif">
+                <i class="nav-icon fas fa-table"></i>
+                <p>
+                    Homework
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+                <li class="nav-item">
+                    <a href="{{ url('teacher/homework/homework') }}"
+                        class="nav-link @if (Request::segment(3) == 'notice_board') active @endif">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Homework</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
 
          <li class="nav-item">
             <a href="{{ url('teacher/my_notice_board') }}"
