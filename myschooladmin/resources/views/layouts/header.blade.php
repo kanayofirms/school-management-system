8ecc270b8fcd57f838ae44e1ccccbf5f6d9d7369
kanayofirms <kanyeze@gmail.com>
2024-03-10 23:52:29 +0100

    Create menu for my homework on student side

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -623,6 +623,16 @@
                 </a>
             </li>
 
+            <li class="nav-item">
+                <a href="{{ url('student/my_homework') }}"
+                    class="nav-link @if (Request::segment(2) == 'my_homework') active @endif">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>
+                        My Homework
+                    </p>
+                </a>
+            </li>
+
              <li class="nav-item">
                  <a href="{{ url('student/account') }}"
                      class="nav-link @if (Request::segment(2) == 'account') active @endif">
