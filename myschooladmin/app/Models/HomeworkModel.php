8e7fd17526619b3d77190c685fd9ff00852662b6
kanayofirms <kanyeze@gmail.com>
2024-03-12 07:52:34 +0100

    Refactor query with whereNotIn to select only for one student in a record

--- myschooladmin/app/Models/HomeworkModel.php
+++ myschooladmin/app/Models/HomeworkModel.php
@@ -130,7 +130,7 @@ class HomeworkModel extends Model
         return $return;
     }
 
-    static public function getRecordStudent($class_id)
+    static public function getRecordStudent($class_id, $student_id)
     {
         $return = self::select('homework.*', 'class.name as class_name', 'subject.name 
         as subject_name', 'users.name as created_by_name')
@@ -138,7 +138,12 @@ class HomeworkModel extends Model
             ->join('class', 'class.id', '=', 'homework.class_id')
             ->join('subject', 'subject.id', '=', 'homework.subject_id')
             ->where('homework.class_id','=', $class_id)
-            ->where('homework.is_delete', '=', 0);
+            ->where('homework.is_delete', '=', 0)
+            ->whereNotIn('homework.id', function($query) use ($student_id) {
+                $query->select('homework_submit.homework_id')
+                        ->from('homework_submit')
+                        ->where('homework_submit.student_id', '=', $student_id);
+            });
 
             if(!empty(Request::get('subject_name')))
             {
