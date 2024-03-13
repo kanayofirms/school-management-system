71ac6d5e73ded0c91cfa9d96ac174d04a8252005
kanayofirms <kanyeze@gmail.com>
2024-03-13 04:47:33 +0100

    Create function to get document and update app

--- myschooladmin/app/Models/HomeworkSubmitModel.php
+++ myschooladmin/app/Models/HomeworkSubmitModel.php
@@ -10,4 +10,25 @@ class HomeworkSubmitModel extends Model
     use HasFactory;
 
     protected $table = "homework_submit";
+
+    static public function getRecordStudent($student_id)
+    {
+        $return = HomeworkSubmitModel::select('homework_submit.*')
+                    ->join('homework', 'homework.id', '=', 'homework_submit.homework_id')
+                    ->orderBy('homework_submit.id', 'desc')
+                    ->paginate(20);
+
+        return $return;
+    }
+
+    public function getDocument()
+    {
+        if(!empty($this->document_file) && file_exists('upload/homework/'.$this->document_file))
+        {
+            return url('upload/homework/'.$this->document_file);
+        }
+        else{
+            return "";
+        }
+    }
 }
