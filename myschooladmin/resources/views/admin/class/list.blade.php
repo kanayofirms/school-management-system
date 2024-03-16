d52b5d39778d4e4a261206654b951356cbad12db
kanayofirms <kanyeze@gmail.com>
2024-03-16 16:31:05 +0100

    Refactor code with amount value and update app

--- myschooladmin/resources/views/admin/class/list.blade.php
+++ myschooladmin/resources/views/admin/class/list.blade.php
@@ -77,6 +77,7 @@
                                         <tr>
                                             <th>#</th>
                                             <th>Name</th>
+                                            <th>Amount</th>
                                             <th>Status</th>
                                             <th>Created By</th>
                                             <th>Created Date</th>
@@ -88,6 +89,7 @@
                                             <tr>
                                                 <td>{{ $value->id }}</td>
                                                 <td>{{ $value->name }}</td>
+                                                <td>{{ number_format($value->amount, 2) }}</td>
                                                 <td>
                                                     @if ($value->status == 0)
                                                         Active
