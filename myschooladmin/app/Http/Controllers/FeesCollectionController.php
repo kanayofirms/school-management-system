1bb38dde78b3aa2994e264e82717b586536763fd
kanayofirms <kanyeze@gmail.com>
2024-03-17 00:34:44 +0100

    Create function to collect fees and update app

--- myschooladmin/app/Http/Controllers/FeesCollectionController.php
+++ myschooladmin/app/Http/Controllers/FeesCollectionController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Models\ClassModel;
 
 class FeesCollectionController extends Controller
 {
-    //
+    public function collect_fees()
+    {
+        $data['getRecord'] = ClassModel::getRecord();
+        $data['header_title'] = "Collect Fees";
+        return view('admin.fees_collection.collect_fees', $data);
+    }
 }
