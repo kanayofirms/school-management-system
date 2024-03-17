a28df6d153c205f90418ba3e312370bd19b9e50f
kanayofirms <kanyeze@gmail.com>
2024-03-17 06:31:39 +0100

    Create foreach loop and get class

--- myschooladmin/resources/views/admin/fees_collection/collect_fees.blade.php
+++ myschooladmin/resources/views/admin/fees_collection/collect_fees.blade.php
@@ -7,18 +7,13 @@
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Class List</h1>
-                    </div>
-                    <div class="col-sm-6" style="text-align: right;">
-                        <a href="{{ url('admin/class/add') }}" class="btn btn-primary">Add New Class</a>
-
+                    <div class="col-sm-12">
+                        <h1>Collect Fees</h1>
                     </div>
 
-                </div><!-- /.container-fluid -->
+                </div>
         </section>
 
-        <!-- Main content -->
         <section class="content">
 
             <div class="container-fluid">
@@ -30,27 +25,51 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Search Class</h3>
+                                <h3 class="card-title">Search Collect Fees Student</h3>
                             </div>
                             <form method="get" action="">
                                 <div class="card-body">
                                     <div class="row">
-                                        <div class="form-group col-md-3">
-                                            <label>Name</label>
-                                            <input type="text" class="form-control" value="{{ Request::get('name') }}"
-                                                name="name" placeholder="Name">
+                                        <div class="form-group col-md-2">
+                                            <label>Class</label>
+                                            <select class="form-control" name="class_id" id="">
+                                                <option value="">Select Class</option>
+                                                @foreach ($getClass as $class)
+                                                <option {{ (Request::get('class_id') == $class->id) ? 'selected' : '' }} 
+                                                    value="{{ $class->id }}">{{ $class->name }}</option>
+                                                    
+                                                @endforeach
+                                            </select>
                                         </div>
 
-                                        <div class="form-group col-md-3">
-                                            <label>Date</label>
-                                            <input type="date" class="form-control" value="{{ Request::get('date') }}"
-                                                name="date" placeholder="Email">
+                                        <div class="form-group col-md-2">
+                                            <label>Student ID</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('student_id') }}"
+                                                name="student_id" placeholder="Student ID">
                                         </div>
 
-                                        <div class="form-group col-md-3">
+                                        <div class="form-group col-md-2">
+                                            <label>Student First Name</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('first_name') }}"
+                                                name="first_name" placeholder="Student First Name">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Student Middle Name</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('middle_name') }}"
+                                                name="middle_name" placeholder="Student Middle Name">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <label>Student Last Name</label>
+                                            <input type="text" class="form-control" value="{{ Request::get('last_name') }}"
+                                                name="last_name" placeholder="Student Last Name">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
                                             <button class="btn btn-primary" type="submit"
                                                 style="margin-top: 30px;">Search</button>
-                                            <a href="{{ url('admin/class/list') }}" class="btn btn-success"
+                                            <a href="{{ url('admin/fees_collection/collect_fees') }}" class="btn btn-success"
                                                 style="margin-top: 30px;">Reset</a>
                                         </div>
 
@@ -68,7 +87,7 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Class List</h3>
+                                <h3 class="card-title">Student List</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body p-0">
@@ -85,33 +104,12 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach ($getRecord as $value)
-                                            <tr>
-                                                <td>{{ $value->id }}</td>
-                                                <td>{{ $value->name }}</td>
-                                                <td>{{ number_format($value->amount, 2) }}</td>
-                                                <td>
-                                                    @if ($value->status == 0)
-                                                        Active
-                                                    @else
-                                                        Inactive
-                                                    @endif
-                                                </td>
-                                                <td>{{ $value->created_by_name }}</td>
-                                                <td>{{ date('d-m-Y H:i A', strtotime($value->created_at)) }}</td>
-                                                <td>
-                                                    <a href="{{ url('admin/class/edit/' . $value->id) }}"
-                                                        class="btn btn-primary">Edit</a>
-                                                    <a href="{{ url('admin/class/delete/' . $value->id) }}"
-                                                        class="btn btn-danger">Delete</a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
+                                        
 
                                     </tbody>
                                 </table>
                                 <div style="padding: 10px; float:right;">
-                                    {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
+                                   
                                 </div>
                             </div>
                             <!-- /.card-body -->
