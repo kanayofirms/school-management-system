a1312057a08bf1ded45c128734e1d135b6fc177c
kanayofirms <kanyeze@gmail.com>
2024-03-17 06:58:05 +0100

    Refactor collect fees function with condition to get record

--- myschooladmin/app/Http/Controllers/FeesCollectionController.php
+++ myschooladmin/app/Http/Controllers/FeesCollectionController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Models\ClassModel;
+use App\Models\User;
+
 
 class FeesCollectionController extends Controller
 {
-    public function collect_fees()
+    public function collect_fees(Request $request)
     {
         $data['getClass'] = ClassModel::getClass();
+
+        if(!empty($request->all()))
+        {
+            $data['getRecord'] = User::getCollectFeesStudent();
+        }
+
         $data['header_title'] = "Collect Fees";
         return view('admin.fees_collection.collect_fees', $data);
     }
