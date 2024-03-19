0d0a4b56f6ca2d537976a24e165f346189be344f
kanayofirms <kanyeze@gmail.com>
2024-03-19 12:27:05 +0100

    Create script code for pop up and refactor code for feasibility

--- myschooladmin/resources/views/admin/fees_collection/add_fees.blade.php
+++ myschooladmin/resources/views/admin/fees_collection/add_fees.blade.php
@@ -1,34 +1,28 @@
 @extends('layouts.app')
 
-
 @section('content')
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <div class="col-sm-12">
+                    <div class="col-sm-6">
                         <h1>Collect Fees</h1>
                     </div>
-
+                    <div class="col-sm-6" style="text-align: right;">
+                        <button type="button" class="btn btn-primary" id="AddFees">Add Fees</button>
+                    </div>
                 </div>
         </section>
-
         <section class="content">
-
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
-
                         @include('_message')
-
-                        <!-- /.card -->
-
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Student List</h3>
+                                <h3 class="card-title">Payment Details</h3>
                             </div>
-                            <!-- /.card-header -->
                             <div class="card-body p-0">
                                 <table class="table table-striped">
                                     <thead>
@@ -56,4 +50,42 @@
             </div>
         </section>
     </div>
+
+    <div class="modal fade" id="AddFeesModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Add Fees</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Amount:</label>
+                  <input type="text" class="form-control" name="amount">
+                </div>
+                <div class="form-group">
+                  <label for="message-text" class="col-form-label">Message:</label>
+                  <textarea class="form-control" id="message-text"></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Send message</button>
+            </div>
+          </div>
+        </div>
+      </div>
+@endsection
+
+@section('script')
+<script type="text/javascript">
+    $('#AddFees').click(function() {
+        $('#AddFeesModal').modal('show');
+    });
+</script>
+
 @endsection
