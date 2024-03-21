e756410f7f81bb86f5af8f9862c341f46db144bb
kanayofirms <kanyeze@gmail.com>
2024-03-21 15:22:15 +0100

    Create fees collection menu on student side

--- myschooladmin/resources/views/layouts/header.blade.php
+++ myschooladmin/resources/views/layouts/header.blade.php
@@ -591,6 +591,16 @@
              </li>
 
              <li class="nav-item">
+                <a href="{{ url('student/fees_collection') }}"
+                    class="nav-link @if (Request::segment(2) == 'fees_collection') active @endif">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>
+                        Fees Collection
+                    </p>
+                </a>
+            </li>
+            
+            <li class="nav-item">
                  <a href="{{ url('student/my_calendar') }}"
                      class="nav-link @if (Request::segment(2) == 'my_calendar') active @endif">
                      <i class="nav-icon far fa-user"></i>
