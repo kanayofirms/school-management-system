339ca5e2f55363cef0f31056713fbd2c040c5e31
kanayofirms <kanyeze@gmail.com>
2024-03-21 21:06:20 +0100

    Refactor where queries with is payment

--- myschooladmin/app/Models/StudentAddFeesModel.php
+++ myschooladmin/app/Models/StudentAddFeesModel.php
@@ -23,6 +23,7 @@ class StudentAddFeesModel extends Model
                     ->join('class', 'class.id', '=', 'student_add_fees.class_id')
                     ->join('users', 'users.id', '=', 'student_add_fees.created_by')
                     ->where('student_add_fees.student_id', '=', $student_id)
+                    ->where('student_add_fees.is_payment', '=', 1)
                     ->get();
     }
 
@@ -30,6 +31,7 @@ class StudentAddFeesModel extends Model
     {
         return self::where('student_add_fees.class_id', '=', $class_id)
                     ->where('student_add_fees.student_id', '=', $student_id)
+                    ->where('student_add_fees.is_payment', '=', 1)
                     ->sum('student_add_fees.paid_amount');
     }
 }
