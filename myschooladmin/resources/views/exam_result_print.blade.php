13f592ac0a5a37d3e26c4c199cdf035a5c804586
kanayofirms <kanyeze@gmail.com>
2024-03-22 08:34:06 +0100

    Create tables for readability and update app

--- myschooladmin/resources/views/exam_result_print.blade.php
+++ myschooladmin/resources/views/exam_result_print.blade.php
@@ -5,9 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Print Exam Result</title>
+    <style type="text/css">
+        .margin-bottom
+        {
+            margin-bottom: 3px;
+        }
+    </style>
 </head>
 <body>
-    <div id="page"></div>
+    <div id="page">
+            <table style="width: 100%; text-align:center;">
+                <tr>
+                    <td width="5%"></td>
+                    <td width="15%"><img style="width: 110px;" src="" alt=""></td>
+                    <td align="left">
+                        <h1>MODERN IDEAL COLLEGE</h1>
+                    </td>
+                </tr>
+            </table>
+
+            <table style="width:100%">
+                    <tr>
+                        <td width="5%"></td>
+                        <td width="70%">
+                            <table class="margin-bottom" style="width: 100%">
+                                <tbody>
+                                    <tr>
+                                        <td width="27%">Name Of Student : </td>
+                                        <td style="border-bottom:1px solid; width:100%;"></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <table class="margin-bottom" style="width: 100%">
+                                <tbody>
+                                    <tr>
+                                        <td width="23%">Admission No : </td>
+                                        <td style="border-bottom:1px solid; width:100%;"></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <table class="margin-bottom" style="width: 100%">
+                                <tbody>
+                                    <tr>
+                                        <td width="23%">Class : </td>
+                                        <td style="border-bottom:1px solid; width:100%;"></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+            </table>
+    </div>
 
     <script type="text/javascript">
         // window.print();
