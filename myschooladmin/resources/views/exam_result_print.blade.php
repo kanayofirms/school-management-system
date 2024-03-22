0702e5db22447baea8a08fe25fea9cfede6b1449
kanayofirms <kanyeze@gmail.com>
2024-03-22 07:12:59 +0100

    Create view for exam result print and update app

---
+++ myschooladmin/resources/views/exam_result_print.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Print Exam Result</title>
+</head>
+<body>
+    <div id="page"></div>
+
+    <script type="text/javascript">
+        // window.print();
+    </script>
+</body>
+</html>
