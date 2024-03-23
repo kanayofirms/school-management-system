a447b1db5ea0d81fdf7208c2f174ec8f41330af3
kanayofirms <kanyeze@gmail.com>
2024-03-23 23:55:03 +0100

    Refactor code for functionality and update app

--- myschooladmin/resources/views/admin/admin/list.blade.php
+++ myschooladmin/resources/views/admin/admin/list.blade.php
@@ -88,8 +88,9 @@
                                             <tr>
                                                 <td>{{ $value->id }}</td>
                                                 <td>
-                                                    @if (!@empty($value->getProfile()))
-                                                    <img src="{{ $value->getProfile() }}" style="height: 50px; width: 50px; border-radius: 50%;">    
+                                                    @if (!@empty($value->getProfileDirect()))
+                                                    <img src="{{ $value->getProfileDirect() }}" 
+                                                        style="height: 50px; width: 50px; border-radius: 50%;">    
                                                     @endif
                                                 </td>
                                                 <td>{{ $value->name }}</td>
