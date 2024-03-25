67de8f09bfd3e2120512fae07eb0292d2805b4ae
kanayofirms <kanyeze@gmail.com>
2024-03-25 03:03:53 +0100

    Set favicon icon in header

--- myschooladmin/resources/views/layouts/app.blade.php
+++ myschooladmin/resources/views/layouts/app.blade.php
@@ -4,7 +4,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ !empty($header_title) ? $header_title : '' }} - Myschooladmin</title>
+  @php
+    $getHeaderSetting = App\Models\SettingModel::getSingle();
+  @endphp
 
+  <link href="{{ $getHeaderSetting->getFavicon() }}" rel="icon" type="image/jpg" />
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
