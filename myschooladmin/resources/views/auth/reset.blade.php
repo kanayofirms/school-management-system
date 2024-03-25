eb8c615b9280b7ea18a1cced514769d9372972ae
kanayofirms <kanyeze@gmail.com>
2024-03-25 03:30:45 +0100

    Set favicon icon in header

--- myschooladmin/resources/views/auth/reset.blade.php
+++ myschooladmin/resources/views/auth/reset.blade.php
@@ -4,6 +4,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Reset Password</title>
+  @php
+    $getHeaderSetting = App\Models\SettingModel::getSingle();
+  @endphp
+  <link href="{{ $getHeaderSetting->getFavicon() }}" rel="icon" type="image/jpg" />
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
