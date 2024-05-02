9b5c3088a998e5abb39c1faf79371fca19c827a8
kanayofirms <kanyeze@gmail.com>
2024-05-02 20:28:50 +0100

    Create menu for totay received payment and update app

--- myschooladmin/resources/views/admin/dashboard.blade.php
+++ myschooladmin/resources/views/admin/dashboard.blade.php
@@ -3,41 +3,33 @@
 
  @section('content')
      <div class="content-wrapper">
-         <!-- Content Header (Page header) -->
          <div class="content-header">
              <div class="container-fluid">
                  <div class="row mb-2">
                     <div class="col-sm-12">
                          <h1 class="m-0">Dashboard</h1>
-                     </div><!-- /.col -->
-                 </div><!-- /.row -->
-             </div><!-- /.container-fluid -->
+                     </div>
+                 </div>
+             </div>
          </div>
-         <!-- /.content-header -->
-
-         <!-- Main content -->
          <section class="content">
              <div class="container-fluid">
-                 <!-- Small boxes (Stat box) -->
                  <div class="row">
                      <div class="col-lg-3 col-6">
-                         <!-- small box -->
                          <div class="small-box bg-info">
                              <div class="inner">
                                  <h3>150</h3>
 
-                                 <p>New Orders</p>
+                                 <p>Today Received Payment</p>
                              </div>
                              <div class="icon">
                                  <i class="ion ion-bag"></i>
                              </div>
-                             <a href="#" class="small-box-footer">More info <i
+                             <a href="{{ url('admin/fees_collection/collect_fees') }}" class="small-box-footer">More info <i
                                      class="fas fa-arrow-circle-right"></i></a>
                          </div>
                      </div>
-                     <!-- ./col -->
                      <div class="col-lg-3 col-6">
-                         <!-- small box -->
                          <div class="small-box bg-success">
                              <div class="inner">
                                  <h3>53<sup style="font-size: 20px">%</sup></h3>
@@ -51,9 +43,7 @@
                                      class="fas fa-arrow-circle-right"></i></a>
                          </div>
                      </div>
-                     <!-- ./col -->
                      <div class="col-lg-3 col-6">
-                         <!-- small box -->
                          <div class="small-box bg-warning">
                              <div class="inner">
                                  <h3>44</h3>
@@ -67,9 +57,7 @@
                                      class="fas fa-arrow-circle-right"></i></a>
                          </div>
                      </div>
-                     <!-- ./col -->
                      <div class="col-lg-3 col-6">
-                         <!-- small box -->
                          <div class="small-box bg-danger">
                              <div class="inner">
                                  <h3>65</h3>
@@ -83,14 +71,9 @@
                                      class="fas fa-arrow-circle-right"></i></a>
                          </div>
                      </div>
-                     <!-- ./col -->
                  </div>
-                 <!-- /.row -->
-                 <!-- Main row --> 
-                 <!-- /.row (main row) -->
-             </div><!-- /.container-fluid -->
+             </div>
          </section>
-         <!-- /.content -->
      </div>
 
      
