df16601d71f6d0d677f2b41dba0744fe67e1aa38
kanayofirms <kanyeze@gmail.com>
2024-05-03 09:07:40 +0100

    Create user dashboards and make dynamic

--- myschooladmin/resources/views/admin/dashboard.blade.php
+++ myschooladmin/resources/views/admin/dashboard.blade.php
@@ -20,7 +20,7 @@
                              <div class="inner">
                                  <h3>150</h3>
 
-                                 <p>Today Received Payment</p>
+                                 <p>All Time Received Payment</p>
                              </div>
                              <div class="icon">
                                  <i class="ion ion-bag"></i>
@@ -29,48 +29,84 @@
                                      class="fas fa-arrow-circle-right"></i></a>
                          </div>
                      </div>
+
+                     <div class="col-lg-3 col-6">
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>150</h3>
+
+                                <p>Today Received Payment</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-bag"></i>
+                            </div>
+                            <a href="{{ url('admin/fees_collection/collect_fees') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    
                      <div class="col-lg-3 col-6">
                          <div class="small-box bg-success">
                              <div class="inner">
-                                 <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                 <h3>{{ $TotalStudent }}</h3>
 
-                                 <p>Bounce Rate</p>
-                             </div>
-                             <div class="icon">
-                                 <i class="ion ion-stats-bars"></i>
-                             </div>
-                             <a href="#" class="small-box-footer">More info <i
-                                     class="fas fa-arrow-circle-right"></i></a>
-                         </div>
-                     </div>
-                     <div class="col-lg-3 col-6">
-                         <div class="small-box bg-warning">
-                             <div class="inner">
-                                 <h3>44</h3>
-
-                                 <p>User Registrations</p>
+                                 <p>Total Student</p>
                              </div>
                              <div class="icon">
                                  <i class="ion ion-person-add"></i>
                              </div>
-                             <a href="#" class="small-box-footer">More info <i
+                             <a href="{{ url('admin/student/list') }}" class="small-box-footer">More info <i
                                      class="fas fa-arrow-circle-right"></i></a>
                          </div>
                      </div>
-                     <div class="col-lg-3 col-6">
-                         <div class="small-box bg-danger">
-                             <div class="inner">
-                                 <h3>65</h3>
 
-                                 <p>Unique Visitors</p>
-                             </div>
-                             <div class="icon">
-                                 <i class="ion ion-pie-graph"></i>
-                             </div>
-                             <a href="#" class="small-box-footer">More info <i
-                                     class="fas fa-arrow-circle-right"></i></a>
-                         </div>
-                     </div>
+                     <div class="col-lg-3 col-6">
+                        <div class="small-box bg-primary">
+                            <div class="inner">
+                                <h3>{{ $TotalTeacher }}</h3>
+
+                                <p>Total Teacher</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="{{ url('admin/teacher/list') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>{{ $TotalParent }}</h3>
+
+                                <p>Total Parent</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="{{ url('admin/parent/list') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>{{ $TotalAdmin }}</h3>
+
+                                <p>Total Admin</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="{{ url('admin/admin/list') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                     
                  </div>
              </div>
          </section>
