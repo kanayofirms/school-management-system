706b4ec0a7eefb3f8f50defc1fa2d2ab225fc778
kanayofirms <kanyeze@gmail.com>
2024-05-03 13:34:28 +0100

    Create join query to get total class from the database and update app

--- myschooladmin/app/Models/ClassModel.php
+++ myschooladmin/app/Models/ClassModel.php
@@ -50,4 +50,15 @@ class ClassModel extends Model
 
         return $return;
     }
+
+    static public function getTotalClass()
+    {
+        $return = ClassModel::select('class.id')
+                    ->join('users', 'users.id', 'class.created_by')
+                    ->where('class.is_delete', '=', 0)
+                    ->where('class.status', '=', 0)
+                    ->count();
+
+        return $return;
+    }
 }
