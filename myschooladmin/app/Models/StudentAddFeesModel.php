3f5a0375fd5061d3601fb2864074bebb271bb1e6
kanayofirms <kanyeze@gmail.com>
2024-05-03 09:49:18 +0100

    Create where query to get total today fees from database

--- myschooladmin/app/Models/StudentAddFeesModel.php
+++ myschooladmin/app/Models/StudentAddFeesModel.php
@@ -34,4 +34,11 @@ class StudentAddFeesModel extends Model
                     ->where('student_add_fees.is_payment', '=', 1)
                     ->sum('student_add_fees.paid_amount');
     }
+
+    static public function getTotalTodayFees()
+    {
+        return self::where('student_add_fees.is_payment', '=', 1)
+                    ->whereDate('student_add_fees.created_at', '=', date('Y-m-d'))
+                    ->sum('student_add_fees.paid_amount');
+    }
 }
