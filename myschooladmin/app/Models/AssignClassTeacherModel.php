12cbb0490ac9cce1cb9cc68362edc6e759b8237f
kanayofirms <kanyeze@gmail.com>
2024-05-05 23:08:49 +0100

    Create join and where queries for get teacher student count function

--- myschooladmin/app/Models/AssignClassTeacherModel.php
+++ myschooladmin/app/Models/AssignClassTeacherModel.php
@@ -64,6 +64,22 @@ class AssignClassTeacherModel extends Model
                     ->get();
     }
 
+    static public function getMyClassSubjectCount($teacher_id)
+    {
+        return AssignClassTeacherModel::select('assign_class_teacher.id')
+                    ->join('class', 'class.id', '=', 'assign_class_teacher.class_id')
+                    ->join('class_subject', 'class_subject.class_id', '=', 'class.id')
+                    ->join('subject', 'subject.id', '=', 'class_subject.subject_id')
+                   ->where('assign_class_teacher.is_delete', '=', 0)
+                   ->where('assign_class_teacher.status', '=', 0)
+                   ->where('subject.status', '=', 0)
+                   ->where('subject.is_delete', '=', 0)
+                   ->where('class_subject.status', '=', 0)
+                   ->where('class_subject.is_delete', '=', 0)
+                   ->where('assign_class_teacher.teacher_id', '=', $teacher_id)
+                    ->count();
+    }
+
     static public function getMyClassSubjectGroup($teacher_id)
     {
         return AssignClassTeacherModel::select('assign_class_teacher.*', 'class.name as 
