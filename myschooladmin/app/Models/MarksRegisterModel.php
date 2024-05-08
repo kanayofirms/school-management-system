63c6db6fefc85a7357d00dddcd2a389cbd852081
kanayofirms <kanyeze@gmail.com>
2024-05-08 13:10:23 +0100

    Refactor code and update app

--- myschooladmin/app/Models/MarksRegisterModel.php
+++ myschooladmin/app/Models/MarksRegisterModel.php
@@ -69,4 +69,15 @@ class MarksRegisterModel extends Model
         return $position !== false ? $position + 1 : null; // Return null if student is not found
     }
 
+    static public function getClass($exam_id, $student_id)
+    {
+        return MarksRegisterModel::select('class.name as class_name')
+            ->join('exam', 'exam.id', '=', 'marks_register.exam_id')
+            ->join('class', 'class.id', '=', 'marks_register.class_id')
+            ->join('subject', 'subject.id', '=', 'marks_register.subject_id')
+            ->where('marks_register.exam_id', '=', $exam_id)
+            ->where('marks_register.student_id', '=', $student_id)
+            ->first();
+    }
+
 }
