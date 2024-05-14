eedac3eaf6007bfa5dc7dc52ff8b02196bf35931
kanayofirms <kanyeze@gmail.com>
2024-05-14 11:18:25 +0100

    Create total homework on student dashboard

--- myschooladmin/resources/views/student/dashboard.blade.php
+++ myschooladmin/resources/views/student/dashboard.blade.php
@@ -30,126 +30,51 @@
                         </div>
                     </div>
 
-                   
                     <div class="col-lg-3 col-6">
                         <div class="small-box bg-success">
                             <div class="inner">
-                                <h3>0</h3>
+                                <h3>{{ $TotalSubject }}</h3>
+ 
+                                <p>Total Subject</p>
+                            </div>
+                            <div class="icon">
+                                <i class="nav-icon fas fa-table"></i>
+                            </div>
+                            <a href="{{ url('student/my_subject') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
 
-                                <p>Total Student</p>
+                   
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>{{ $TotalNoticeBoard }}</h3>
+
+                                <p>Total Notice Board</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-person-add"></i>
                             </div>
-                            <a href="{{ url('admin/student/list') }}" class="small-box-footer">More info <i
+                            <a href="{{ url('student/my_notice_board') }}" class="small-box-footer">More info <i
                                     class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-6">
-                       <div class="small-box bg-primary">
-                           <div class="inner">
-                               <h3>0</h3>
+                        <div class="small-box bg-primary">
+                            <div class="inner">
+                                <h3>0</h3>
 
-                               <p>Total Teacher</p>
-                           </div>
-                           <div class="icon">
-                               <i class="ion ion-person-add"></i>
-                           </div>
-                           <a href="{{ url('admin/teacher/list') }}" class="small-box-footer">More info <i
-                                   class="fas fa-arrow-circle-right"></i></a>
-                       </div>
-                   </div>
-
-                   <div class="col-lg-3 col-6">
-                       <div class="small-box bg-warning">
-                           <div class="inner">
-                               <h3>0</h3>
-
-                               <p>Total Parent</p>
-                           </div>
-                           <div class="icon">
-                               <i class="ion ion-person-add"></i>
-                           </div>
-                           <a href="{{ url('admin/parent/list') }}" class="small-box-footer">More info <i
-                                   class="fas fa-arrow-circle-right"></i></a>
-                       </div>
-                   </div>
-
-                   <div class="col-lg-3 col-6">
-                       <div class="small-box bg-danger">
-                           <div class="inner">
-                               <h3>0</h3>
-
-                               <p>Total Admin</p>
-                           </div>
-                           <div class="icon">
-                               <i class="ion ion-person-add"></i>
-                           </div>
-                           <a href="{{ url('admin/admin/list') }}" class="small-box-footer">More info <i
-                                   class="fas fa-arrow-circle-right"></i></a>
-                       </div>
-                   </div>
-
-                   <div class="col-lg-3 col-6">
-                       <div class="small-box bg-danger">
-                           <div class="inner">
-                               <h3>0</h3>
-
-                               <p>Total Exam</p>
-                           </div>
-                           <div class="icon">
-                               <i class="nav-icon fas fa-table"></i>
-                           </div>
-                           <a href="{{ url('admin/examinations/exam/list') }}" class="small-box-footer">More info <i
-                                   class="fas fa-arrow-circle-right"></i></a>
-                       </div>
-                   </div>
-
-                   <div class="col-lg-3 col-6">
-                       <div class="small-box bg-warning">
-                           <div class="inner">
-                               <h3>0</h3>
-
-                               <p>Total Class</p>
-                           </div>
-                           <div class="icon">
-                               <i class="nav-icon fas fa-table"></i>
-                           </div>
-                           <a href="{{ url('admin/class/list') }}" class="small-box-footer">More info <i
-                                   class="fas fa-arrow-circle-right"></i></a>
-                       </div>
-                   </div>
-
-                   <div class="col-lg-3 col-6">
-                       <div class="small-box bg-success">
-                           <div class="inner">
-                               <h3>0</h3>
-
-                               <p>Total Subject</p>
-                           </div>
-                           <div class="icon">
-                               <i class="nav-icon fas fa-table"></i>
-                           </div>
-                           <a href="{{ url('admin/subject/list') }}" class="small-box-footer">More info <i
-                                   class="fas fa-arrow-circle-right"></i></a>
-                       </div>
-                   </div>
-
-                   <div class="col-lg-3 col-6">
-                       <div class="small-box bg-success">
-                           <div class="inner">
-                               <h3>0</h3>
-
-                               <p>Total Users</p>
-                           </div>
-                           <div class="icon">
-                               <i class="ion ion-person-add"></i>
-                           </div>
-                           <a href="#" class="small-box-footer">More info<i
-                                   class="fas fa-arrow-circle-right"></i></a>
-                       </div>
-                   </div>
+                                <p>Total Homework</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="{{ url('student/my_notice_board') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
