9145623cfa60fc8df95c5d3f392feb3751fd91cd
kanayofirms <kanyeze@gmail.com>
2024-05-14 09:41:36 +0100

    Create where queries for total paid amount for the student and update app

--- myschooladmin/app/Models/StudentAddFeesModel.php
+++ myschooladmin/app/Models/StudentAddFeesModel.php
@@ -47,4 +47,11 @@ class StudentAddFeesModel extends Model
         return self::where('student_add_fees.is_payment', '=', 1)
                     ->sum('student_add_fees.paid_amount');
     }
+
+    static public function TotalPaidAmountStudent($student_id)
+    {
+        return self::where('student_add_fees.is_payment', '=', 1)
+                    ->where('student_add_fees.student_id', '=', $student_id)
+                    ->sum('student_add_fees.paid_amount');
+    }
 }
